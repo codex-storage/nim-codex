b779af23608cc75924a3c2839b38d61c2a30a3a9
Mark Spanbroek <mark@spanbroek.net>
2021-01-21 17:10:20 +0100

    Add initializers for bitswap messages

---
+++ tests/ipfs/testBitswapMessages.nim
@@ -0,0 +1,23 @@
+import std/unittest
+import pkg/libp2p
+import pkg/ipfs/protobuf/bitswap
+import pkg/ipfs/bitswap/messages
+import ../helpers/examples
+
+suite "bitswap messages":
+
+  test "creates message with want list":
+    let cid1, cid2 = Cid.example
+    let message = Message.want(cid1, cid2)
+    check message == Message(wantlist: WantList(entries: @[
+      Entry(`block`: cid1.data.buffer),
+      Entry(`block`: cid2.data.buffer)
+    ]))
+
+  test "creates message that sends blocks":
+    let block1, block2 = seq[byte].example
+    let message = Message.send(block1, block2)
+    check message == Message(payload: @[
+      Block(data: block1),
+      Block(data: block2)
+    ])
--- tests/testAll.nim
+++ tests/testAll.nim
@@ -3,6 +3,7 @@ import ./ipfs/testChunking
 import ./ipfs/testRepo
 import ./ipfs/testDhtRouting
 import ./ipfs/testProtobuf
+import ./ipfs/testBitswapMessages
 import ./ipfs/testIpfs
 
 {.warning[UnusedImport]: off.}
