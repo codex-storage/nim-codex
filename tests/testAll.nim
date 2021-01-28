a63000f602bdb6dd8fc08f936675e66ab3ce7f1c
Mark Spanbroek <mark@spanbroek.net>
2021-01-28 17:40:16 +0100

    Exchange objects between peers using bitswap

--- tests/testAll.nim
+++ tests/testAll.nim
@@ -5,6 +5,7 @@ import ./ipfs/testDhtRouting
 import ./ipfs/testProtobuf
 import ./ipfs/testBitswapMessages
 import ./ipfs/testBitswapProtocol
+import ./ipfs/testBitswap
 import ./ipfs/testIpfs
 
 {.warning[UnusedImport]: off.}
