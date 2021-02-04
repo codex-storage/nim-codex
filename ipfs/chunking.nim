1e8f9900a56fd33251ca3a9ce61aa1204f493963
Dmitriy Ryajov <dryajov@gmail.com>
2021-02-03 18:33:41 -0600

    adding chunker

--- ipfs/chunking.nim
+++ ipfs/chunking.nim
@@ -1,10 +1,49 @@
-import ./ipfsobject
+## Nim-Dagger
+## Copyright (c) 2021 Status Research & Development GmbH
+## Licensed under either of
+##  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+##  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+## at your option.
+## This file may not be copied, modified, or distributed except according to
+## those terms.
 
-export ipfsobject
+import ./blocktype
 
-proc createObject*(file: File): IpfsObject =
-  let contents = file.readAll()
-  IpfsObject(data: cast[seq[byte]](contents))
+export blocktype
 
-proc writeToFile*(obj: IpfsObject, output: File) =
-  output.write(cast[string](obj.data))
+const
+  DefaultBlockSize: int64 = 1024 * 256
+
+type
+  # default reader type
+  Reader* = proc(maxSize: int): seq[byte] {.gcsafe, closure.}
+
+  ChunkerType {.pure.} = enum
+    SizedChunker
+    RabinChunker
+
+  Chunker = object of RootObj
+    reader: Reader
+    case kind: ChunkerType:
+    of SizedChunker:
+      size*: int
+      pad*: bool # pad last block if less than size
+    of RabinChunker:
+      min*, max*: int
+
+proc getBytes*(c: Chunker): seq[byte] =
+  var bytes = c.reader(c.size)
+  if bytes.len < c.size and c.pad:
+    bytes.setLen(c.size)
+
+  return bytes
+
+proc newFixedSizeChunker*(
+  T: type Chunker,
+  reader: Reader,
+  size = DefaultBlockSize,
+  pad = false): T =
+  Chunker(
+    kind: ChunkerType.SizedChunker,
+    size: size,
+    pad: pad)
