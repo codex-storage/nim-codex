8823cd5eb4c29187631c56258d8d70724a672bed
Dmitriy Ryajov <dryajov@gmail.com>
2021-02-05 09:49:17 -0600

    rename ipfs to dagger

--- dagger/bitswap/bitswap.nim
+++ dagger/bitswap/bitswap.nim
@@ -7,9 +7,13 @@
 ## This file may not be copied, modified, or distributed except according to
 ## those terms.
 
+import std/sugar
+import std/hashes
 import std/tables
 import std/sequtils
 import std/heapqueue
+
+import pkg/chronicles
 import pkg/chronos
 import pkg/libp2p
 import pkg/libp2p/errors
@@ -25,19 +29,32 @@ const
   DefaultTimeout = 500.milliseconds
 
 type
-  BitswapInfo* = object
-    sentHaves: seq[Cid]                # peers we've sent WANTs recently
-    peersHave: seq[Cid]                # remote peers have lists
-    peersWants: seq[Cid]               # remote peers want lists
+  BitswapInfo* = ref object
+    id: PeerID
+    sentHaves: seq[Cid] # peers we've sent WANTs recently
+    peerHave: seq[Cid]  # remote peers have lists
+    peerWants: seq[Cid] # remote peers want lists
 
   Bitswap* = ref object of BlockProvider
     store: BlockStore
     network: BitswapNetwork
-    peers: Table[PeerID, BitswapInfo]
+    peers: seq[BitswapInfo]
     wantList: seq[Cid]                                # our pending wants
     # TODO: use a heapqueue instead of seq
-    session: seq[PeerID]                              # peers currently exchanging blocks
+    session: seq[BitswapInfo]                         # peers currently exchanging blocks
     waitList: Table[PeerID, Table[Cid, Future[void]]] # pending WANT requests
+
+# TODO: move to libp2p
+proc hash*(cid: Cid): Hash {.inline.} =
+  hash(cid.data.buffer)
+
+proc contains*(a: openarray[BitswapInfo], b: PeerID): bool {.inline.} =
+  a.filterIt( it.id == b ).len > 0
+
+proc getInfo(b: Bitswap, peerId: PeerID): BitswapInfo {.inline} =
+  let peer = b.peers.filterIt( it.id == peerId )
+  if peer.len > 0:
+    return peer[0]
 
 proc addBlockEvent(
   b: Bitswap,
@@ -65,10 +82,10 @@ proc createSession(b: Bitswap, cid: Cid) {.async.} =
   if cid notin b.wantList:
     b.wantList.add(cid)
 
-  let pending: seq[Future[void]]
-  for p in toSeq(b.peers.keys):
-    pending.add(b.addBlockEvent(p, cid))
-    asyncCheck b.network.sendWantList(p, cid)
+  var pending: seq[Future[PeerID]]
+  for p in b.peers:
+    pending.add(b.addBlockEvent(p.id, cid))
+    asyncCheck b.network.sendWantList(p.id, cid)
 
   let peerFuts = await allFinished(pending)
   var peers: seq[PeerID]
@@ -86,20 +103,21 @@ proc getBlockFromSession(b: Bitswap, cid: Cid): Future[bt.Block] {.async.} =
 
   if b.session.len <= 0:
     warn "Couldn't get any peers to get blocks from"
+    return
 
   let blockPeer = b.session[0] # TODO: this should be a heapqueu
   # get the block from the best peer
   await b.network.sendWantList(
-    blockPeer,
+    blockPeer.id,
     cid,
     wantType = WantType.wantBlock)
 
-  await checkFutures(
-    allFinished(b.session[1..b.session.high]
-    .map(proc(id: PeerID) =
-      let info = h.peers[peer]
-      if cid notin info.sentHaves[id]:
-        b.network.sendWantList(id, cid, wantType = WantType.wantHave))))
+  proc sendWants(info: BitswapInfo) {.async.} =
+    if cid notin info.sentHaves:
+      await b.network.sendWantList(info.id, cid, wantType = WantType.wantHave)
+
+  checkFutures(
+    await allFinished(b.session[1..b.session.high].map(sendWants)))
 
 method getBlock*(b: Bitswap, cid: Cid): Future[bt.Block] =
   discard
@@ -112,15 +130,25 @@ proc setupPeer(b: Bitswap, peer: PeerID) =
   ## list exchange
   ##
 
+  if peer notin b.peers:
+    b.peers.add(BitswapInfo(
+      id: peer
+    ))
+
 proc dropPeer(b: Bitswap, peer: PeerID) =
   ## Cleanup disconnected peer
   ##
 
-  b.peers.del(peer)
-  if waiting in toSeq(b.waitList.values):
+  # check if there are any pending wants
+  # or blocks and cancel them
+  if peer in b.waitList:
+    let waiting = b.waitList[peer]
     for f in toSeq(waiting.values):
       if not f.finished:
         f.cancel()
+
+  # drop the peer from the peers table
+  b.peers.keepItIf( it.id != peer )
 
 proc new*(T: type Bitswap, store: BlockStore, network: BitswapNetwork): T =
 
@@ -129,10 +157,12 @@ proc new*(T: type Bitswap, store: BlockStore, network: BitswapNetwork): T =
 
   store.addChangeHandler(onBlocks)
 
+  let waitList = initTable[Table[PeerID, Table[Cid, Future[void]]]]
   let b = Bitswap(
     store: store,
     network: network,
-    waitList: initTable[Table[PeerID, Table[Cid, Future[void]]]])
+    waitList: waitList
+    )
 
   proc peerEventHandler(peerId: PeerID, event: PeerEvent) {.async.} =
     if event.kind == PeerEventKind.Joined:
@@ -143,13 +173,20 @@ proc new*(T: type Bitswap, store: BlockStore, network: BitswapNetwork): T =
   network.switch.addPeerEventHandler(peerEventHandler, PeerEventKind.Joined)
   network.switch.addPeerEventHandler(peerEventHandler, PeerEventKind.Left)
 
-  proc wantListHandler(peer: PeerID, wantList: WantList) {.gcsafe.} =
-    for list in wantList.entries:
-      let cid = Cid.init(list.`block`)
-      var info = b.peers[peer]
-      if peer in b.waitList:
-        var list = b.waitList[peer]
-        if cid in list:
-          list[cid].complete(peer)
+  proc blockPresenceHandler(peer: PeerID, precense: seq[BlockPresence]) {.gcsafe.} =
+    for b in precense:
+      let cid = Cid.init(b.cid)
+      var info = b.getInfo(peer)
+
+      for list in b.wantList:
+        if not isNil(info):
+          # notify listeners of new want or block
+          if cid in info.waitList:
+            var list = info.waitList[cid]
+            info.waitList[cid].complete(peer)
+
+          if cid notin info.peerHave:
+            if b.type == BlockPresenceType.presenceHave:
+              info.peerHave.add(cid)
 
   return b
