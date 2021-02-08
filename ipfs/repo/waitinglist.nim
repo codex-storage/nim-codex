ac16d0224a0503fc802edd359deef3815c733e4b
Mark Spanbroek <mark@spanbroek.net>
2021-02-08 11:58:40 +0100

    Remove futures from waiting list when timeouts occur

--- ipfs/repo/waitinglist.nim
+++ ipfs/repo/waitinglist.nim
@@ -1,20 +1,32 @@
 import std/tables
+import std/sequtils
+import std/sugar
 import pkg/chronos
 
 type WaitingList*[T] = ref object
   futures: Table[T, seq[Future[void]]]
 
-proc wait*[T](list: var WaitingList, item: T, timeout: Duration): Future[void] =
+proc remove[T](list: WaitingList[T], item: T, future: Future[void]) =
+  list.futures[item].keepIf(x => x != future)
+  if list.futures[item].len == 0:
+    list.futures.del(item)
+
+proc wait*[T](list: WaitingList[T], item: T, timeout: Duration): Future[void] =
   let future = newFuture[void]("waitinglist.wait")
   proc onTimeout(_: pointer) =
     if not future.finished:
       future.complete()
+      list.remove(item, future)
   discard setTimer(Moment.fromNow(timeout), onTimeout, nil)
   list.futures.mgetOrPut(item, @[]).add(future)
   future
 
-proc deliver*[T](list: var WaitingList, item: T) =
+proc deliver*[T](list: WaitingList[T], item: T) =
   if list.futures.hasKey(item):
     for future in list.futures[item]:
       future.complete()
     list.futures.del(item)
+
+proc count*[T](list: WaitingList[T]): int =
+  for x in list.futures.values:
+    result += x.len
--- tests/ipfs/testWaitingList.nim
+++ tests/ipfs/testWaitingList.nim
@@ -29,3 +29,28 @@ suite "waiting list":
     check not wait.finished
     await sleepAsync(100.milliseconds)
     check wait.finished
+
+  test "timeout does nothing when item already delivered":
+    let wait = list.wait("apple", 100.milliseconds)
+    list.deliver("apple")
+    await sleepAsync(100.milliseconds)
+    check wait.finished
+
+  test "tracks the amount of waiting futures":
+    check list.count == 0
+    asyncSpawn list.wait("apple", 1.minutes)
+    check list.count == 1
+    asyncSpawn list.wait("orange", 1.minutes)
+    check list.count == 2
+    asyncSpawn list.wait("apple", 1.minutes)
+    check list.count == 3
+
+  test "removes future when item is delivered":
+    asyncSpawn list.wait("apple", 1.minutes)
+    list.deliver("apple")
+    check list.count == 0
+
+  test "removes future after timeout":
+    asyncSpawn list.wait("apple", 100.milliseconds)
+    await sleepAsync(100.milliseconds)
+    check list.count == 0
