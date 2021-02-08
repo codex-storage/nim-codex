8e76ecfa9feb8e88a5598d9b15f838dd6e10aa11
Mark Spanbroek <mark@spanbroek.net>
2021-02-08 14:19:00 +0100

    Fix: adapt to latest libp2p api
    Fixes compilation error.

--- tests/ipfs/testDhtRouting.nim
+++ tests/ipfs/testDhtRouting.nim
@@ -1,13 +1,12 @@
 import std/unittest
-import pkg/libp2p
 import pkg/ipfs/ipfsobject
 import pkg/ipfs/dht/routing
 
 suite "DHT routing table":
 
   test "finds peers closest to some content":
-    let peer1 = PeerInfo(peer: PeerId(data: @[1'u8]))
-    let peer2 = PeerInfo(peer: PeerId(data: @[2'u8]))
+    let peer1 = PeerInfo(peerId: PeerId(data: @[1'u8]))
+    let peer2 = PeerInfo(peerId: PeerId(data: @[2'u8]))
     let contentId = IpfsObject(data: @[]).cid
 
     var table = RoutingTable()
