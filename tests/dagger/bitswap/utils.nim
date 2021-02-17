e80911706a9859fb8f4dd5406eb98cff753b2009
Dmitriy Ryajov <dryajov@gmail.com>
2021-02-17 15:28:03 -0600

    adding helpers for bitswap tests

---
+++ tests/dagger/bitswap/utils.nim
@@ -0,0 +1,34 @@
+import std/sequtils
+
+import pkg/chronos
+import pkg/libp2p
+
+import pkg/dagger/bitswap
+import pkg/dagger/stores/memorystore
+import pkg/dagger/blocktype as bt
+
+proc generateNodes*(
+  num: Natural,
+  blocks: openArray[bt.Block] = [],
+  secureManagers: openarray[SecureProtocol] = [
+    SecureProtocol.Noise,
+  ]): seq[tuple[switch: Switch, bitswap: Bitswap]] =
+
+  for i in 0..<num:
+    let
+      switch = newStandardSwitch()
+      network = BitswapNetwork.new(switch = switch)
+      bitswap = Bitswap.new(MemoryStore.new(blocks), network)
+
+    switch.mount(network)
+
+    # initialize our want lists
+    bitswap.engine.wantList = blocks.mapIt( it.cid )
+    switch.mount(network)
+    result.add((switch, bitswap))
+
+proc connectNodes*(nodes: seq[Switch]) {.async.} =
+  for dialer in nodes:
+    for node in nodes:
+      if dialer.peerInfo.peerId != node.peerInfo.peerId:
+        await dialer.connect(node.peerInfo.peerId, node.peerInfo.addrs)
