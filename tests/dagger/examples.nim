84656604ec6b4de226a3ad351fba88a71f73bb84
Mark Spanbroek <mark@spanbroek.net>
2021-04-07 16:53:00 +0200

    Fix tests on 32 bit platforms

--- tests/dagger/examples.nim
+++ tests/dagger/examples.nim
@@ -10,10 +10,14 @@ proc example*(_: type UInt256): UInt256 =
     b = rand(byte)
   UInt256.fromBytes(bytes)
 
+proc example*(_: type UInt48): UInt48 =
+  # workaround for https://github.com/nim-lang/Nim/issues/17670
+  uint64.rand mod (UInt48.high + 1)
+
 proc example*(_: type SignedState): SignedState =
   var wallet = Wallet.init(EthPrivateKey.random())
   let hub, asset, receiver = EthAddress.example
   let chainId, amount = UInt256.example
-  let nonce = UInt48.rand()
+  let nonce = UInt48.example
   let channel = wallet.openLedgerChannel(hub, chainId, nonce, asset, amount).get
   wallet.pay(channel, asset, receiver, amount).get
