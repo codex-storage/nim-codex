06766f25aab48d850fe2e53f5390f8147dd242de
Mark Spanbroek <mark@spanbroek.net>
2021-04-08 09:22:41 +0200

    Move pricing and payments into bitswap protobuf message
    Only a single message type is forwarded to the rpc handler,
    so we add pricing and payment info to that message type.

--- dagger/bitswap/protobuf/bitswap.nim
+++ dagger/bitswap/protobuf/bitswap.nim
@@ -17,6 +17,7 @@ import_proto3 "message.proto"
 export Message
 export Wantlist, WantType, Entry
 export Block, BlockPresenceType, BlockPresence
+export PricingMessage, StateChannelUpdate
 
 proc hash*(e: Entry): Hash =
   hash(e.`block`)
