4a21d30cdfe937572f5717b4288c7c9baf779965
Mark Spanbroek <mark@spanbroek.net>
2021-04-26 16:34:04 +0200

    Add pricing to block presence messages

--- tests/testAll.nim
+++ tests/testAll.nim
@@ -2,6 +2,7 @@ import ./dagger/bitswap/testbitswap
 import ./dagger/bitswap/testengine
 import ./dagger/bitswap/testnetwork
 import ./dagger/bitswap/protobuf/testpayments as testprotobufpayments
+import ./dagger/bitswap/protobuf/testpresence
 import ./dagger/bitswap/engine/testpayments as testenginepayments
 import ./dagger/testasyncheapqueue
 import ./dagger/testblockstore
