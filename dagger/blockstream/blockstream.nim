00b5aa9c55c3b0cd694a59835e1901b49a787a68
Dmitriy Ryajov <dryajov@gmail.com>
2021-08-27 11:44:43 -0600

    move blockstream into own dir

---
+++ dagger/blockstream/blockstream.nim
@@ -0,0 +1,29 @@
+## Nim-Dagger
+## Copyright (c) 2021 Status Research & Development GmbH
+## Licensed under either of
+##  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+##  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+## at your option.
+## This file may not be copied, modified, or distributed except according to
+## those terms.
+
+{.push raises: [Defect].}
+
+import pkg/questionable
+import pkg/questionable/results
+import ../blocktype
+
+export blocktype
+
+type
+  BlockStreamRef* = ref object of RootObj
+
+method nextBlock*(b: BlockStreamRef): ?!Block {.base.} =
+  doAssert(false, "Not implemented!")
+
+iterator items*(b: BlockStreamRef): Block =
+  while true:
+    without blk =? b.nextBlock():
+      break
+
+    yield blk
