8b0ee9651a07a89d5c009bb4afaafed9c61737f9
Dmitriy Ryajov <dryajov@gmail.com>
2021-12-13 17:36:45 -0600

    wip: adding node tests

---
+++ tests/dagger/testnode.nim
@@ -0,0 +1,89 @@
+import std/os
+import std/sequtils
+import std/algorithm
+
+import pkg/asynctest
+import pkg/chronos
+import pkg/stew/byteutils
+
+import pkg/nitro
+import pkg/libp2p
+import pkg/libp2p/errors
+
+import pkg/dagger/utils/asyncfutures
+import pkg/dagger/rng
+import pkg/dagger/stores
+import pkg/dagger/blockexchange
+import pkg/dagger/chunker
+import pkg/dagger/node
+import pkg/dagger/conf
+import pkg/dagger/manifest
+import pkg/dagger/blocktype as bt
+
+import ./helpers
+import ./examples
+
+suite "Test Node":
+
+  var
+    switch: Switch
+    wallet: WalletRef
+    network: BlockExcNetwork
+    localStore: MemoryStore
+    engine: BlockExcEngine
+    store: NetworkStore
+    node: DaggerNodeRef
+
+  setup:
+    switch = newStandardSwitch()
+    wallet = WalletRef.new(EthPrivateKey.random())
+    network = BlockExcNetwork.new(switch)
+    localStore = MemoryStore.new()
+    engine = BlockExcEngine.new(localStore, wallet, network)
+    store = NetworkStore.new(engine, localStore)
+    node = DaggerNodeRef.new(switch, store, engine)
+
+    await node.start()
+
+  teardown:
+    await node.stop()
+
+  test "Test store":
+    let
+      (path, _, _) = instantiationInfo(-2, fullPaths = true) # get this file's name
+      file = open(path.splitFile().dir /../ "fixtures" / "test.jpg")
+      chunker = FileChunker.new(file = file, chunkSize = 4096)
+
+    let
+      stream = BufferStream.new()
+      storeFut = node.store(stream)
+
+    without var manifest =? BlocksManifest.init():
+      fail()
+
+    try:
+      for chunkFut in chunker.toStream():
+        let
+          chunk = await chunkFut
+          blk = bt.Block.new(chunk)
+
+        manifest.put(blk.cid)
+        await stream.pushData(chunk)
+    finally:
+      await stream.pushEof()
+      await stream.close()
+
+    without manifestCid =? (await storeFut):
+      fail()
+
+    check manifestCid in localStore
+
+    without manifestBlock =? await localStore.getBlock(manifestCid):
+      fail()
+
+    without var localManifest =? BlocksManifest.init(manifestBlock):
+      fail()
+
+    echo "MANIFEST ", manifest.blocks
+    echo "LOCAL MANIFEST ", localManifest.blocks
+    # check manifest.treeHash() == localManifest.treeHash()
