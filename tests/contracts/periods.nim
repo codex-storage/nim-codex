49322a1c6b19cd29f4d122d5a15028ee9119b701
Mark Spanbroek <mark@spanbroek.net>
2022-03-17 14:15:04 +0100

    [contracts] Update to new marketplace design

---
+++ tests/contracts/periods.nim
@@ -0,0 +1,16 @@
+import pkg/stint
+
+type
+  Periodicity* = object
+    seconds*: UInt256
+  Period* = UInt256
+  Timestamp* = UInt256
+
+func periodOf*(periodicity: Periodicity, timestamp: Timestamp): Period =
+  timestamp div periodicity.seconds
+
+func periodStart*(periodicity: Periodicity, period: Period): Timestamp =
+  period * periodicity.seconds
+
+func periodEnd*(periodicity: Periodicity, period: Period): Timestamp =
+  periodicity.periodStart(period + 1)
