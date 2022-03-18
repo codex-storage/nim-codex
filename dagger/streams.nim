ac01ff66b21a3704df1b90992f685804675ec2c0
Dmitriy Ryajov <dryajov@gmail.com>
2022-03-18 12:18:34 -0600

    adding a "seekable" base stream

---
+++ dagger/streams.nim
@@ -0,0 +1,4 @@
+import ./streams/seekablestream
+import ./streams/storestream
+
+export seekablestream, storestream
---
+++ dagger/streams/seekablestream.nim
@@ -0,0 +1,27 @@
+## Nim-Dagger
+## Copyright (c) 2022 Status Research & Development GmbH
+## Licensed under either of
+##  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+##  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+## at your option.
+## This file may not be copied, modified, or distributed except according to
+## those terms.
+
+import pkg/libp2p
+import pkg/chronos
+import pkg/chronicles
+
+export libp2p, chronos, chronicles
+
+logScope:
+  topics = "dagger seekablestream"
+
+type
+  SeekableStream* = ref object of LPStream
+    offset*: int
+
+method size*(self: SeekableStream): int {.base.} =
+  raiseAssert("method unimplemented")
+
+proc setPos*(self: SeekableStream, pos: int) =
+  self.offset = pos
