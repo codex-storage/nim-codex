5f2a379af0472a38e477f6d87c9e81a9625846e1
Dmitriy Ryajov <dryajov@gmail.com>
2022-04-19 19:22:08 -0600

    rework moc discovery

--- tests/dagger/mockdiscovery.nim
+++ tests/dagger/mockdiscovery.nim
@@ -17,9 +17,9 @@ import pkg/libp2pdht/discv5/protocol as discv5
 export discv5
 
 type
-  Discovery* = ref object
-    findBlockProviders_var*: proc(d: Discovery, cid: Cid): seq[SignedPeerRecord] {.gcsafe.}
-    publishProvide_var*: proc(d: Discovery, cid: Cid) {.gcsafe.}
+  MockDiscovery* = ref object of Discovery
+    findBlockProvidersHandler*: proc(d: MockDiscovery, cid: Cid): seq[SignedPeerRecord] {.gcsafe.}
+    publishProvideHandler*: proc(d: MockDiscovery, cid: Cid) {.gcsafe.}
 
 proc new*(
   T: type Discovery,
@@ -38,13 +38,13 @@ proc findPeer*(
 proc findBlockProviders*(
   d: Discovery,
   cid: Cid): Future[seq[SignedPeerRecord]] {.async.} =
-  if isNil(d.findBlockProviders_var): return
+  if isNil(d.findBlockProvidersHandler): return
 
-  return d.findBlockProviders_var(d, cid)
+  return d.findBlockProvidersHandler(d, cid)
 
-proc publishProvide*(d: Discovery, cid: Cid) {.async.} =
-  if isNil(d.publishProvide_var): return
-  d.publishProvide_var(d, cid)
+method provideBlock*(d: MockDiscovery, cid: Cid) {.async.} =
+  if isNil(d.publishProvideHandler): return
+  d.publishProvideHandler(d, cid)
 
 
 proc start*(d: Discovery) {.async.} =
