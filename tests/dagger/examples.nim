8cbd542adae962f3edecb27d20600208963dd299
Dmitriy Ryajov <dryajov@gmail.com>
2022-05-16 10:55:25 -0600

    fix compilation

--- tests/dagger/examples.nim
+++ tests/dagger/examples.nim
@@ -5,7 +5,7 @@ import pkg/nitro
 import pkg/stint
 import pkg/dagger/rng
 import pkg/dagger/stores
-import pkg/dagger/blocktype
+import pkg/dagger/blocktype as bt
 import pkg/dagger/sales
 import ../examples
 
@@ -38,10 +38,10 @@ proc example*(_: type Pricing): Pricing =
     price: uint32.rand.u256
   )
 
-proc example*(_: type Block): Block =
+proc example*(_: type bt.Block): bt.Block =
   let length = rand(4096)
   let bytes = newSeqWith(length, rand(uint8))
-  Block.new(bytes).tryGet()
+  bt.Block.new(bytes).tryGet()
 
 proc example*(_: type PeerId): PeerID =
   let key = PrivateKey.random(Rng.instance[]).get
@@ -51,7 +51,7 @@ proc example*(_: type BlockExcPeerCtx): BlockExcPeerCtx =
   BlockExcPeerCtx(id: PeerID.example)
 
 proc example*(_: type Cid): Cid =
-  Block.example.cid
+  bt.Block.example.cid
 
 proc example*(_: type Availability): Availability =
   Availability.init(uint16.example, uint16.example, uint64.example.u256)
