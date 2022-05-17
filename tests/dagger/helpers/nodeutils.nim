a8c22fbe872ca6715ed50e1522dfbd55fd58b2bb
Dmitriy Ryajov <dryajov@gmail.com>
2022-05-17 15:10:40 -0600

    expose all node components

--- tests/dagger/helpers/nodeutils.nim
+++ tests/dagger/helpers/nodeutils.nim
@@ -10,10 +10,22 @@ import pkg/dagger/blockexchange
 
 import ../examples
 
+type
+  NodesComponents* = tuple[
+    switch: Switch,
+    blockDiscovery: Discovery,
+    wallet: WalletRef,
+    network: BlockExcNetwork,
+    localStore: BlockStore,
+    peerStore: PeerCtxStore,
+    pendingBlocks: PendingBlocksManager,
+    discovery: DiscoveryEngine,
+    engine: BlockExcEngine,
+    networkStore: NetworkStore]
+
 proc generateNodes*(
   num: Natural,
-  blocks: openArray[bt.Block] = []):
-  seq[tuple[switch: Switch, blockexc: NetworkStore]] =
+  blocks: openArray[bt.Block] = []): seq[NodesComponents] =
   for i in 0..<num:
     let
       switch = newStandardSwitch(transportFlags = {ServerFlags.ReuseAddr})
@@ -28,7 +40,17 @@ proc generateNodes*(
       networkStore = NetworkStore.new(engine, localStore)
 
     switch.mount(network)
-    result.add((switch, networkStore))
+    result.add((
+      switch,
+      blockDiscovery,
+      wallet,
+      network,
+      localStore,
+      peerStore,
+      pendingBlocks,
+      discovery,
+      engine,
+      networkStore))
 
 proc connectNodes*(nodes: seq[Switch]) {.async.} =
   for dialer in nodes:
