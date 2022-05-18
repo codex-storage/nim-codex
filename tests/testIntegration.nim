15a571387f14c1da45b850cb544d7c85258db4b2
Mark Spanbroek <mark@spanbroek.net>
2022-05-18 16:49:09 +0200

    [test] Give integration test a bit more time to complete
    For slow windows CI runners.

--- tests/testIntegration.nim
+++ tests/testIntegration.nim
@@ -89,7 +89,7 @@ ethersuite "Integration tests":
       client.post(baseurl1 & "/upload", "some file contents").body
 
     proc buy(cid: string): string =
-      let expiry = ((waitFor provider.currentTime()) + 5).toHex
+      let expiry = ((waitFor provider.currentTime()) + 10).toHex
       let json = %*{"duration": "0x100", "maxPrice": "0x400", "expiry": expiry}
       client.post(baseurl1 & "/storage/request/" & cid, $json).body
 
