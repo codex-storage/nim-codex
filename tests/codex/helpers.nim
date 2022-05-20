143fcb465e765b3b654dfa8c91003c822be79c5c
Dmitriy Ryajov <dryajov@gmail.com>
2022-05-20 14:41:18 -0600

    move delete blocks helper to helpers

--- tests/codex/helpers.nim
+++ tests/codex/helpers.nim
@@ -1,6 +1,10 @@
+import pkg/chronos
 import pkg/libp2p
 import pkg/libp2p/varint
-import pkg/codex/blocktype
+import pkg/codex/blocktype as bt
+import pkg/codex/stores
+import pkg/codex/manifest
+import pkg/codex/rng
 
 import ./helpers/nodeutils
 import ./helpers/randomchunker
@@ -10,8 +14,8 @@ export randomchunker, nodeutils
 # NOTE: The meaning of equality for blocks
 # is changed here, because blocks are now `ref`
 # types. This is only in tests!!!
-func `==`*(a, b: Block): bool =
-  (a.cid == b.cid) and (a.data == b.data)
+func `==`*(a, b: bt.Block): bool =
+  (a.cid == b.cid) and (a.data == b.  data)
 
 proc lenPrefix*(msg: openArray[byte]): seq[byte] =
   ## Write `msg` with a varint-encoded length prefix
@@ -23,3 +27,32 @@ proc lenPrefix*(msg: openArray[byte]): seq[byte] =
   buf[vbytes.len..<buf.len] = msg
 
   return buf
+
+proc deleteBlocks(
+  store: BlockStore,
+  manifest: Manifest,
+  blks, bytes: int) {.async.} =
+  var pos: seq[int]
+  while true:
+    if pos.len >= blks:
+      break
+
+    var i = -1
+    if (i = Rng.instance.rand(manifest.len - 1); pos.find(i) >= 0):
+      continue
+
+    pos.add(i)
+    var
+      blk = (await store.getBlock(manifest[i])).tryGet()
+      bytePos: seq[int]
+
+    while true:
+      if bytePos.len > bytes:
+        break
+
+      var ii = -1
+      if (ii = Rng.instance.rand(blk.data.len - 1); bytePos.find(ii) >= 0):
+        continue
+
+      bytePos.add(ii)
+      blk.data[ii] = byte 0
--- tests/codex/storageproofs/testpor.nim
+++ tests/codex/storageproofs/testpor.nim
@@ -17,32 +17,6 @@ const
   QueryLen = 22
   DataSetSize = BlockSize * 100
 
-proc deleteBlocks(store: BlockStore, manifest: Manifest, blks, bytes: int) {.async.} =
-  var pos: seq[int]
-  while true:
-    if pos.len >= blks:
-      break
-
-    var i = -1
-    if (i = Rng.instance.rand(manifest.len - 1); pos.find(i) >= 0):
-      continue
-
-    pos.add(i)
-    var
-      blk = (await store.getBlock(manifest[i])).tryGet()
-      bytePos: seq[int]
-
-    while true:
-      if bytePos.len > bytes:
-        break
-
-      var ii = -1
-      if (ii = Rng.instance.rand(blk.data.len - 1); bytePos.find(ii) >= 0):
-        continue
-
-      bytePos.add(ii)
-      blk.data[ii] = byte 0
-
 suite "BLS PoR":
   let
     chunker = RandomChunker.new(Rng.instance(), size = DataSetSize, chunkSize = BlockSize)
