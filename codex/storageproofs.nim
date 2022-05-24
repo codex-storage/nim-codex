67959fea6af3c1e6b658014661c6262ec3d45563
Dmitriy Ryajov <dryajov@gmail.com>
2022-05-24 15:28:09 -0600

    rename porstore to stpstore

--- codex/storageproofs.nim
+++ codex/storageproofs.nim
@@ -1,5 +1,6 @@
 import ./storageproofs/por
 import ./storageproofs/timing
 import ./storageproofs/stpstore
+import ./storageproofs/stpnetwork
 
-export por, timing, stpstore
+export por, timing, stpstore, stpnetwork
