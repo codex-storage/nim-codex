9c1542497a129745f0390cc6ddf7a4facb3f0d66
Dmitriy Ryajov <dryajov@gmail.com>
2022-05-25 14:55:14 -0600

    export stpproto

--- codex/storageproofs.nim
+++ codex/storageproofs.nim
@@ -2,5 +2,6 @@ import ./storageproofs/por
 import ./storageproofs/timing
 import ./storageproofs/stpstore
 import ./storageproofs/stpnetwork
+import ./storageproofs/stpproto
 
-export por, timing, stpstore, stpnetwork
+export por, timing, stpstore, stpnetwork, stpproto
