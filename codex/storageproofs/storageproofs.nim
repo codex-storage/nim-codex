57e969c71f0db78fa5ca011b1dc33b7f75502ae7
Dmitriy Ryajov <dryajov@gmail.com>
2022-05-25 16:36:43 -0600

    handle tag uppload

--- codex/storageproofs/storageproofs.nim
+++ codex/storageproofs/storageproofs.nim
@@ -20,16 +20,17 @@ import ../utils
 
 import ./por
 import ./stpnetwork
+import ./stpproto
 import ./stpstore
 import ./timing
 
-export stpnetwork, stpstore, por, timing
+export stpnetwork, stpstore, por, timing, stpproto
 
 type
   StorageProofs* = object
     store*: BlockStore
     network*: StpNetwork
-    porStore*: StpStore
+    stpStore*: StpStore
 
 proc upload*(
   self: StorageProofs,
@@ -39,7 +40,7 @@ proc upload*(
   ## Upload authenticators
   ##
 
-  without por =? (await self.porStore.retrieve(cid)):
+  without por =? (await self.stpStore.retrieve(cid)):
     trace "Unable to retrieve por data from store", cid
     return failure("Unable to retrieve por data from store")
 
@@ -72,22 +73,26 @@ proc setupProofs*(
       spk,
       manifest.blockSize)
 
-  return await self.porStore.store(por, cid)
+  return await self.stpStore.store(por.toMessage(), cid)
 
 proc init*(
   T: type StorageProofs,
   network: StpNetwork,
   store: BlockStore,
-  porStore: StpStore): StorageProofs =
+  stpStore: StpStore): StorageProofs =
 
   var
     self = T(
       store: store,
-      porStore: porStore,
+      stpStore: stpStore,
       network: network)
 
   proc tagsHandler(msg: TagsMessage) {.async, gcsafe.} =
-    discard
+    try:
+      await self.stpStore.store(msg.cid, msg.tags).tryGet()
+      trace "Stored tags", cid = $msg.cid, tags = msg.tags.len
+    except CatchableError as exc:
+      trace "Exception attempting to store tags", exc = exc.msg
 
   self.network.tagsHandler = tagsHandler
   self
