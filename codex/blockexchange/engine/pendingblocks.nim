4f1a2dae058a6e1351272067562b86e5f87b772a
Csaba Kiraly <csaba.kiraly@gmail.com>
2022-10-07 00:04:01 +0200

    fix crash when can't find a CID and timeout hits
    the exception was not raised, thus a Nil was returned that was
    than wrapped in an option, leading to crash.
    
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- codex/blockexchange/engine/pendingblocks.nim
+++ codex/blockexchange/engine/pendingblocks.nim
@@ -51,6 +51,8 @@ proc getWantHandle*(
     raise exc
   except CatchableError as exc:
     trace "Pending WANT failed or expired", exc = exc.msg
+    # no need to cancel, it is already cancelled by wait()
+    raise exc
   finally:
     p.blocks.del(cid)
 
