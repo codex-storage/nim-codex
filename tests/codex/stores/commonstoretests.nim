d8a9f2939fae872d2627d9513f33c4757c76fe9b
Dmitriy Ryajov <dryajov@gmail.com>
2022-11-18 17:33:33 -0600

    adding store's common tests

---
+++ tests/codex/stores/commonstoretests.nim
@@ -0,0 +1,83 @@
+import std/sequtils
+import std/strutils
+import std/options
+
+import pkg/chronos
+import pkg/asynctest
+import pkg/libp2p
+import pkg/stew/byteutils
+import pkg/questionable/results
+import pkg/codex/stores/cachestore
+import pkg/codex/chunker
+
+import ../helpers
+
+type
+  StoreProvider* = proc(): BlockStore {.gcsafe.}
+
+proc commonBlockStoreTests*(name: string, provider: StoreProvider) =
+  suite name & " Store Common":
+    var
+      newBlock, newBlock1, newBlock2, newBlock3: Block
+      store: BlockStore
+
+    setup:
+      newBlock = Block.new("New Kids on the Block".toBytes()).tryGet()
+      newBlock1 = Block.new("1".repeat(100).toBytes()).tryGet()
+      newBlock2 = Block.new("2".repeat(100).toBytes()).tryGet()
+      newBlock3 = Block.new("3".repeat(100).toBytes()).tryGet()
+      store = provider()
+
+    teardown:
+      await store.close()
+
+    test "putBlock":
+      (await store.putBlock(newBlock1)).tryGet()
+      check (await store.hasBlock(newBlock1.cid)).tryGet()
+
+    test "getBlock":
+      (await store.putBlock(newBlock)).tryGet()
+      let blk = await store.getBlock(newBlock.cid)
+      check blk.tryGet() == newBlock
+
+    test "fail getBlock":
+      expect BlockNotFoundError:
+        discard (await store.getBlock(newBlock.cid)).tryGet()
+
+    test "hasBlock":
+      (await store.putBlock(newBlock)).tryGet()
+
+      check:
+        (await store.hasBlock(newBlock.cid)).tryGet()
+        await newBlock.cid in store
+
+    test "fail hasBlock":
+      check:
+        not (await store.hasBlock(newBlock.cid)).tryGet()
+        not (await newBlock.cid in store)
+
+    test "delBlock":
+      (await store.putBlock(newBlock1)).tryGet()
+      check (await store.hasBlock(newBlock1.cid)).tryGet()
+
+      (await store.delBlock(newBlock1.cid)).tryGet()
+
+      check not (await store.hasBlock(newBlock1.cid)).tryGet()
+
+    test "listBlocks Blocks":
+      let
+        blocks = @[newBlock1, newBlock2, newBlock3]
+
+        putHandles = await allFinished(
+          blocks.mapIt( store.putBlock( it ) ))
+
+      for handle in putHandles:
+        check not handle.failed
+        check handle.read.isOK
+
+      let
+        cids = (await store.listBlocks(blockType = BlockType.Block)).tryGet()
+
+      for c in cids:
+        if cid =? (await c):
+          check (await store.hasBlock(cid)).tryGet()
