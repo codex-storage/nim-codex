512cebd6205c166bf69a2840b25c078f90d6eed8
Dmitriy Ryajov <dryajov@gmail.com>
2022-11-18 14:33:13 -0600

    wip

--- codex/stores.nim
+++ codex/stores.nim
@@ -1,7 +1,6 @@
 import ./stores/cachestore
 import ./stores/blockstore
 import ./stores/networkstore
-import ./stores/fsstore
 import ./stores/repostore
 
-export cachestore, blockstore, networkstore, repostore, fsstore
+export cachestore, blockstore, networkstore, repostore
