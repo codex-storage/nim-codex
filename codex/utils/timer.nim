392e7889795f2f2fa64a2894e8a77fd8e293460b
Ben Bierens <thatbenbierens@gmail.com>
2023-02-09 13:44:18 +0100

    Implements timer utility

--- codex/utils/timer.nim
+++ codex/utils/timer.nim
@@ -9,10 +9,46 @@
 
 
 ## Timer
-## Used to execute a callback in a loop or one-shot
+## Used to execute a callback in a loop
 
-type 
+import pkg/chronos
+import pkg/chronicles
+
+type
+  TimerCallback* = proc(): void {.gcsafe.}
   Timer* = ref object
+    callback: TimerCallback
+    interval: Duration
+    name: string
+    isRunning: bool
 
-method start*(timer: Timer, callback: Proc, interval: int) =
-  callback()
+proc new*(T: type Timer, callback: TimerCallback, interval: Duration, timerName = "Unnamed Timer"): T =
+  T(
+    callback: callback,
+    interval: interval,
+    name: timerName,
+    isRunning: false
+  )
+
+proc timerLoop(timer: Timer) {.async.} =
+  try:
+    while timer.isRunning:
+      await sleepAsync(timer.interval)
+      timer.callback()
+  except Exception as exc:
+    timer.isRunning = false
+    error "Timer: ", timer.name, " caught unhandled exception: ", exc
+
+method start*(timer: Timer) =
+  if timer.isRunning:
+    return
+  trace "Timer starting: ", timer.name
+  timer.isRunning = true
+  asyncSpawn timerLoop(timer)
+
+method stop*(timer: Timer) =
+  if not timer.isRunning:
+    return
+
+  trace "Timer stopping: ", timer.name
+  timer.isRunning = false
--- tests/codex/utils/testtimer.nim
+++ tests/codex/utils/testtimer.nim
@@ -8,6 +8,8 @@ import ../helpers/eventually
 
 
 suite "Timer":
+  var timer1: Timer
+  var timer2: Timer
   var output: string
   var numbersState = 0
   var lettersState = 'a'
@@ -20,14 +22,45 @@ suite "Timer":
     output &= $lettersState
     inc lettersState
 
+  proc exceptionCallback() =
+    raise newException(Defect, "Test Exception")
+
   setup:
+    timer1 = Timer.new(numbersCallback, 10.milliseconds)
+    timer2 = Timer.new(lettersCallback, 10.milliseconds)
+
     output = ""
     numbersState = 0
     lettersState = 'a'
 
-  test "Start timer1 should execute callback":
-    let t1 = Timer.new()
-    t1.start(numbersCallback, 1.seconds)
+  teardown:
+    timer1.stop()
+    timer2.stop()
 
+  test "Start timer1 should execute callback":
+    timer1.start()
     check eventually output == "0"
-    
+
+  test "Start timer1 should execute callback multiple times":
+    timer1.start()
+    check eventually output == "012"
+
+  test "Stop timer1 should stop execution of the callback":
+    timer1.start()
+    check eventually output == "012"
+    timer1.stop()
+    await sleepAsync(30.milliseconds)
+    let stoppedOutput = output
+    await sleepAsync(30.milliseconds)
+    check output == stoppedOutput
+
+  test "Exceptions raised in timer callback are handled":
+    let timer = Timer.new(exceptionCallback, 10.milliseconds)
+    timer.start()
+    await sleepAsync(30.milliseconds)
+    timer.stop()
+
+  test "Starting both timers should execute callbacks sequentially":
+    timer1.start()
+    timer2.start()
+    check eventually output == "0a1b2c3d4e"
