2a303d393303688844ec6e927b79d3ba46c07c5e
Mark Spanbroek <mark@spanbroek.net>
2023-04-13 12:23:37 +0200

    [integration] integration test for validator

--- tests/integration/testproofs.nim
+++ tests/integration/testproofs.nim
@@ -1,45 +1,97 @@
+import std/os
 import codex/contracts/marketplace
 import codex/contracts/deployment
+import codex/periods
 import ../contracts/time
 import ../codex/helpers/eventually
 import ./twonodes
 
 twonodessuite "Proving integration test", debug1=false, debug2=false:
 
+  let validatorDir = getTempDir() / "CodexValidator"
+
   var marketplace: Marketplace
-  var config: MarketplaceConfig
+  var period: uint64
 
   setup:
     let deployment = Deployment.init()
     marketplace = Marketplace.new(!deployment.address(Marketplace), provider)
-    config = await marketplace.config()
+    period = (await marketplace.config()).proofs.period.truncate(uint64)
 
     # Our Hardhat configuration does use automine, which means that time tracked by `provider.currentTime()` is not
     # advanced until blocks are mined and that happens only when transaction is submitted.
     # As we use in tests provider.currentTime() which uses block timestamp this can lead to synchronization issues.
     await provider.advanceTime(1.u256)
 
-  proc waitUntilPurchaseIsStarted {.async.} =
-    discard client2.postAvailability(size=0xFFFFF, duration=200, minPrice=300, maxCollateral=200)
-    let expiry = (await provider.currentTime()) + 30
+  proc waitUntilPurchaseIsStarted(proofProbability: uint64 = 3,
+                                  duration: uint64 = 100 * period,
+                                  expiry: uint64 = 30) {.async.} =
+    discard client2.postAvailability(
+      size=0xFFFFF,
+      duration=duration,
+      minPrice=300,
+      maxCollateral=200
+    )
     let cid = client1.upload("some file contents")
-    let purchase = client1.requestStorage(cid, duration=100, reward=400, proofProbability=3, expiry=expiry, collateral=100)
+    let expiry = (await provider.currentTime()) + expiry.u256
+    let purchase = client1.requestStorage(
+      cid,
+      expiry=expiry,
+      duration=duration,
+      proofProbability=proofProbability,
+      collateral=100,
+      reward=400
+    )
     check eventually client1.getPurchase(purchase){"state"} == %"started"
 
-  test "hosts submit periodic proofs for slots they fill":
-    await waitUntilPurchaseIsStarted()
+  proc advanceToNextPeriod {.async.} =
+    let periodicity = Periodicity(seconds: period.u256)
+    let currentPeriod = periodicity.periodOf(await provider.currentTime())
+    let endOfPeriod = periodicity.periodEnd(currentPeriod)
+    await provider.advanceTimeTo(endOfPeriod)
 
+  proc startValidator: NodeProcess =
+    startNode([
+      "--data-dir=" & validatorDir,
+      "--api-port=8089",
+      "--disc-port=8099",
+      "--validator",
+      "--eth-account=" & $accounts[2]
+    ], debug = false)
+
+  proc stopValidator(node: NodeProcess) =
+    node.stop()
+    removeDir(validatorDir)
+
+  test "hosts submit periodic proofs for slots they fill":
+    await waitUntilPurchaseIsStarted(proofProbability=1)
     var proofWasSubmitted = false
     proc onProofSubmitted(event: ProofSubmitted) =
       proofWasSubmitted = true
     let subscription = await marketplace.subscribe(ProofSubmitted, onProofSubmitted)
+    await provider.advanceTime(period.u256)
+    check eventually proofWasSubmitted
+    await subscription.unsubscribe()
+
+  test "validator will mark proofs as missing":
+    let validator = startValidator()
+    await waitUntilPurchaseIsStarted(proofProbability=1)
+
+    node2.stop()
+
+    var slotWasFreed = false
+    proc onSlotFreed(event: SlotFreed) =
+      slotWasFreed = true
+    let subscription = await marketplace.subscribe(SlotFreed, onSlotFreed)
 
     for _ in 0..<100:
-      if proofWasSubmitted:
+      if slotWasFreed:
         break
       else:
-        await provider.advanceTime(config.proofs.period)
+        await advanceToNextPeriod()
         await sleepAsync(1.seconds)
 
-    check proofWasSubmitted
+    check slotWasFreed
+
     await subscription.unsubscribe()
+    stopValidator(validator)
