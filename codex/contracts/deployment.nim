0c970052d9113c8c2e2449760bec97837e63eace
Adam Uhlíř <adam@uhlir.dev>
2023-05-02 16:50:14 +0200

    fix: chainid serialization

--- codex/contracts/deployment.nim
+++ codex/contracts/deployment.nim
@@ -3,6 +3,7 @@ import std/os
 import std/tables
 import pkg/ethers
 import pkg/questionable
+import pkg/chronicles
 
 import ../conf
 import ./marketplace
@@ -19,18 +20,21 @@ const knownAddresses = {
 }.toTable
 
 proc getKnownAddress(T: type, chainId: UInt256): ?Address =
-  if not ($chainId in knownAddresses):
+  let id = chainId.toString(10)
+  notice "Looking for well-known contract addres with ChainID ", chainId=id
+
+  if not (id in knownAddresses):
     return none Address
 
-  return knownAddresses[$chainId].getOrDefault($T, Address.none)
+  return knownAddresses[id].getOrDefault($T, Address.none)
 
 proc new*(_: type Deployment, provider: Provider, config: CodexConf): Deployment =
   Deployment(provider: provider, config: config)
 
 proc address*(deployment: Deployment, contract: type): Future[?Address] {.async.} =
-  let chainId = await deployment.provider.getChainId()
   when contract is Marketplace:
     if address =? deployment.config.marketplaceAddress:
       return some address
 
+  let chainId = await deployment.provider.getChainId()
   return contract.getKnownAddress(chainId)
