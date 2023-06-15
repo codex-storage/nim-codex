3b0277837034744895ab5a8406836e9028d8e95b
benbierens <thatbenbierens@gmail.com>
2023-06-15 08:42:41 +0200

    Removes unused debug support

--- codex/rest/api.nim
+++ codex/rest/api.nim
@@ -46,33 +46,6 @@ proc validate(
   {.gcsafe, raises: [Defect].} =
   0
 
-proc formatEnginePeers(peers: OrderedTable[PeerId, BlockExcPeerCtx]): JsonNode =
-  let jarray = newJArray()
-  for key, value in peers:
-    jarray.add(%*
-      {
-        "peerId": $key,
-        "context": {
-          "blocks": value.blocks.len,
-          "peerWants": value.peerWants.len,
-          "exchanged": value.exchanged,
-          "lastExchange": $(value.lastExchange),
-        }
-      })
-
-  return jarray
-
-proc formatSwitchPeers(peers: Table[PeerId, seq[MultiAddress]]): JsonNode =
-  let jarray = newJArray()
-  for key, value in peers:
-    jarray.add(%*
-      {
-        "peerId": $key,
-        "numberOfAddresses": $value.len
-      })
-
-  return jarray
-
 proc initRestApi*(node: CodexNodeRef, conf: CodexConf): RestRouter =
   var router = RestRouter.init(validate)
   router.api(
@@ -272,8 +245,6 @@ proc initRestApi*(node: CodexNodeRef, conf: CodexConf): RestRouter =
               node.discovery.dhtRecord.get.toURI
             else:
               "",
-          "enginePeers": formatEnginePeers(node.engine.peers.peers),
-          "switchPeers": formatSwitchPeers(node.switch.peerStore[AddressBook].book),
           "codex": {
             "version": $codexVersion,
             "revision": $codexRevision
--- docker/debug/buildsetup.bat
+++
@@ -1,2 +0,0 @@
-docker build -t thatbenbierens/codexsetup:latest -f setup.Dockerfile ..
-docker push thatbenbierens/codexsetup:latest
