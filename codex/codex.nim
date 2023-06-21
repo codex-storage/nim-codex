7a078f71c00a873ff35b44592c5411d0f99ed4af
Jaremy Creechley <creechley@gmail.com>
2023-06-20 18:39:02 -0700

    remove rng prefix - it appears to work now

--- codex/codex.nim
+++ codex/codex.nim
@@ -151,7 +151,7 @@ proc new*(
     .new()
     .withPrivateKey(privateKey)
     .withAddresses(config.listenAddrs)
-    .withRng(rng.Rng.instance())
+    .withRng(Rng.instance())
     .withNoise()
     .withMplex(5.minutes, 5.minutes)
     .withMaxConnections(config.maxPeers)
