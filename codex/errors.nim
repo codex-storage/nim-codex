6f4ac0b42647077eb776fb96c389b2e24f7444ce
Jaremy Creechley <creechley@gmail.com>
2023-07-26 15:41:58 -0700

    convert mapFailure to procs to work around type resolution issues

--- codex/errors.nim
+++ codex/errors.nim
@@ -9,15 +9,22 @@
 
 import pkg/stew/results
 
+export results
+
 type
   CodexError* = object of CatchableError # base codex error
   CodexResult*[T] = Result[T, ref CodexError]
 
-template mapFailure*(
-    exp: untyped,
-    exc: typed = type CodexError
-): untyped =
+proc mapFailure*[T, V, E](
+    exp: Result[T, V],
+    exc: typedesc[E],
+): Result[T, ref E] =
   ## Convert `Result[T, E]` to `Result[E, ref CatchableError]`
   ##
 
-  ((exp.mapErr do (e: auto) -> ref CatchableError: (ref exc)(msg: $e)))
+  proc convertToErr(e: V): ref E =
+    (ref exc)(msg: $e)
+  exp.mapErr(convertToErr)
+
+proc mapFailure*[T, V](exp: Result[T, V]): Result[T, ref CodexError] =
+  mapFailure(exp, CodexError)
