7361ae7584eacf21f4ef5e6307c91d1279155088
Jaremy Creechley <creechley@gmail.com>
2023-07-27 15:04:41 -0700

    update keys

--- tests/codex/utils/testkeyutils.nim
+++ tests/codex/utils/testkeyutils.nim
@@ -1,7 +1,7 @@
 import std/unittest
 import std/os
 import pkg/libp2p except setup
-import pkg/questionable/results
+import pkg/questionable
 import codex/utils/keyutils
 import ../helpers
 
@@ -18,17 +18,17 @@ checksuite "keyutils":
     os.removeDir(path)
 
   test "creates a key file when it does not exist yet":
-    check setupKey(path / "keyfile").isSuccess
+    check setupKey(path / "keyfile").isOk
     check fileExists(path / "keyfile")
 
   test "stores key in a file that's only readable by the user":
-    discard !setupKey(path / "keyfile")
+    discard setupKey(path / "keyfile").get()
     when defined(posix):
       check getFilePermissions(path / "keyfile") == {fpUserRead, fpUserWrite}
     when defined(windows):
       check checkCurrentUserOnlyACL(path / "keyfile").get()
 
   test "reads key file when it does exist":
-    let key = !setupKey(path / "keyfile")
-    check !setupKey(path / "keyfile") == key
+    let key = setupKey(path / "keyfile").get()
+    check setupKey(path / "keyfile").get() == key
 
