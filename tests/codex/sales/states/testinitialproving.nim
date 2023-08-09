e7728f92fa67276acf9c53f65427ee317971aee4
Adam Uhlíř <adam@uhlir.dev>
2023-08-09 07:45:45 +0200

    test: initial proving state tests

--- tests/codex/sales/states/testinitialproving.nim
+++ tests/codex/sales/states/testinitialproving.nim
@@ -1,30 +1,44 @@
-# import std/unittest
-# import pkg/questionable
-# import pkg/codex/contracts/requests
-# import pkg/codex/sales/states/proving
-# import pkg/codex/sales/states/cancelled
-# import pkg/codex/sales/states/failed
-# import pkg/codex/sales/states/filled
-# import ../../examples
-#
-# suite "sales state 'initialproving'":
-#
-#   let request = StorageRequest.example
-#   let slotIndex = (request.ask.slots div 2).u256
-#   var state: SaleInitialProving
-#
-#   setup:
-#     state = SaleInitialProving.new()
-#
-#   test "switches to cancelled state when request expires":
-#     let next = state.onCancelled(request)
-#     check !next of SaleCancelled
-#
-#   test "switches to failed state when request fails":
-#     let next = state.onFailed(request)
-#     check !next of SaleFailed
-#
-#   test "switches to filled state when slot is filled":
-#     let next = state.onSlotFilled(request.id, slotIndex)
-#     check !next of SaleFilled
-#
+import pkg/asynctest
+import pkg/questionable
+import pkg/chronos
+import pkg/codex/contracts/requests
+import pkg/codex/sales/states/initialproving
+import pkg/codex/sales/states/cancelled
+import pkg/codex/sales/states/failed
+import pkg/codex/sales/states/filling
+import pkg/codex/sales/salesagent
+import pkg/codex/sales/salescontext
+import ../../examples
+import ../../helpers
+
+asyncchecksuite "sales state 'initialproving'":
+
+  let proof = exampleProof()
+  let request = StorageRequest.example
+  let slotIndex = (request.ask.slots div 2).u256
+
+  var state: SaleInitialProving
+  var agent: SalesAgent
+
+  setup:
+    let onProve = proc (slot: Slot): Future[seq[byte]] {.async.} =
+                            return proof
+    let context = SalesContext(onProve: onProve.some)
+    agent = newSalesAgent(context,
+                          request.id,
+                          slotIndex,
+                          request.some)
+    state = SaleInitialProving.new()
+
+  test "switches to cancelled state when request expires":
+    let next = state.onCancelled(request)
+    check !next of SaleCancelled
+
+  test "switches to failed state when request fails":
+    let next = state.onFailed(request)
+    check !next of SaleFailed
+
+  test "switches to filled state when slot is filled":
+    let next = await state.run(agent)
+    check !next of SaleFilling
+    check SaleFilling(!next).proof == proof
