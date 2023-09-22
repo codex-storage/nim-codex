2537add13a4cb314a8506e3851b901bcb217e688
Eric <5089238+emizzle@users.noreply.github.com>
2023-09-22 20:00:52 +1000

    ethers: update revert reason retreival

--- codex/sales/states/filling.nim
+++ codex/sales/states/filling.nim
@@ -37,5 +37,5 @@ method run(state: SaleFilling, machine: Machine): Future[?State] {.async.} =
 
   debug "Filling slot", requestId = $data.requestId, slotIndex
   market.cancelOnError:
-    await market.fillSlot(data.requestId, slotIndex, state.proof, collateral)
+    await market.fillSlot(data.requestId, slotIndex, state.proof, collateral).confirm(1)
     debug "Waiting for slot filled event...", requestId = $data.requestId, slotIndex
