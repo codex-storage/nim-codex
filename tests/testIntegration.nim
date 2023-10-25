4dffb643a401351c1b93b3be46d0bd768af6e886
Eric <5089238+emizzle@users.noreply.github.com>
2023-10-25 17:31:04 +1100

    comment out other integration tests for now

--- tests/testIntegration.nim
+++ tests/testIntegration.nim
@@ -1,6 +1,6 @@
-import ./integration/testcli
-import ./integration/testIntegration
-import ./integration/testblockexpiration
+# import ./integration/testcli
+# import ./integration/testIntegration
+# import ./integration/testblockexpiration
 import ./integration/testproofs
 
 {.warning[UnusedImport]:off.}
