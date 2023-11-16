ff89817053c8632cf704e9d77c3cbba335f848cc
benbierens <thatbenbierens@gmail.com>
2023-11-16 13:12:01 +0100

    Review comments by Slava

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,9 +6,18 @@ if [[ "${NAT_IP_AUTO}" == "true" ]]; then
   echo "Internal: Set CODEX_NAT: ${CODEX_NAT}"
 fi
 
-if [[ "${NAT_PUBLIC_IP_AUTO}" == "true" ]]; then
-  export CODEX_NAT=$(curl https://ipinfo.io/ip)
-  echo "Public: Set CODEX_NAT: ${CODEX_NAT}"
+# Get Public IP
+if [[ -n "${NAT_PUBLIC_IP_AUTO}" ]]; then
+  # Waith 60 seconds
+  WAIT=60
+  SECONDS=0
+  while (( SECONDS < WAIT )); do
+    export CODEX_NAT=$(curl -m 5 "${NAT_PUBLIC_IP_AUTO}")
+    # Check the exit code is 0 and returned value is not empty
+    [[ $? -eq 0 && -n "${CODEX_NAT}" ]] && { echo "Public: Set CODEX_NAT: ${CODEX_NAT}"; break; }
+    # Sleep and check again
+    sleep 5
+  done
 fi
 
 # If marketplace is enabled from the testing environment,
