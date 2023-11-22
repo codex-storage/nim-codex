f767d2b4d07f03adb3ef76d94238ba09f2cbb0bb
benbierens <thatbenbierens@gmail.com>
2023-11-22 12:43:49 +0100

    Implements getting treeCID from slot

--- codex/proof/slotblocks.nim
+++ codex/proof/slotblocks.nim
@@ -2,15 +2,29 @@ import std/bitops
 import std/sugar
 
 import pkg/chronos
+import pkg/chronicles
 import pkg/libp2p
 import pkg/questionable
 import pkg/questionable/results
 
 import ../contracts/requests
 import ../stores/blockstore
+import ../manifest
 
 proc getTreeCidForSlot*(slot: Slot, blockstore: BlockStore): Future[?!Cid] {.async.} =
-  raiseAssert("a")
+  without manifestBlockCid =? Cid.init(slot.request.content.cid).mapFailure, err:
+    error "Unable to init CID from slot.content.cid"
+    return failure err
+
+  without manifestBlock =? await blockstore.getBlock(manifestBlockCid), err:
+    error "Failed to fetch manifest block", cid = manifestBlockCid
+    return failure err
+
+  without manifest =? Manifest.decode(manifestBlock):
+    error "Unable to decode manifest"
+    return failure("Unable to decode manifest")
+
+  return success(manifest.treeCid)
 
 proc getSlotBlock*(slot: Slot, blockstore: BlockStore, treeCid: Cid, slotBlockIndex: int): Future[?!Block] {.async.} =
   raiseAssert("a")
