90d4a25c88e08a5b018111980372a416005cf518
benbierens <thatbenbierens@gmail.com>
2023-11-26 17:05:11 +0100

    almost there

--- codex/proof/indexing.nim
+++ codex/proof/indexing.nim
@@ -8,13 +8,13 @@ import types
 # SlotBlockIndex => The index of a Block within a Slot.
 # DatasetBlockIndex => The index of a Block within a Dataset.
 
-proc getSlotBlockIndexForSlotCellIndex*(cellIndex: DSSlotCellIndex, blockSize: uint64): uint64 =
+proc getSlotBlockIndexForSlotCellIndex*(slotCellIndex: DSSlotCellIndex, blockSize: uint64): uint64 =
   let numberOfCellsPerBlock = blockSize div CellSize
-  return cellIndex div numberOfCellsPerBlock
+  return slotCellIndex div numberOfCellsPerBlock
 
-proc getBlockCellIndexForSlotCellIndex*(cellIndex: DSSlotCellIndex, blockSize: uint64): uint64 =
+proc getBlockCellIndexForSlotCellIndex*(slotCellIndex: DSSlotCellIndex, blockSize: uint64): uint64 =
   let numberOfCellsPerBlock = blockSize div CellSize
-  return cellIndex mod numberOfCellsPerBlock
+  return slotCellIndex mod numberOfCellsPerBlock
 
 proc getDatasetBlockIndexForSlotBlockIndex*(slot: Slot, blockSize: uint64, slotBlockIndex: uint64): uint64 =
   let
