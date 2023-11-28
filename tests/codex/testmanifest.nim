1f299f59f6a71535465a940d6f03ebf7af1d8270
benbierens <thatbenbierens@gmail.com>
2023-11-28 20:27:38 +0100

    Adds test for encoding/decoding protected manifest

--- tests/codex/testmanifest.nim
+++ tests/codex/testmanifest.nim
@@ -13,12 +13,32 @@ import ./helpers
 import ./examples
 
 checksuite "Manifest":
-  test "Should encode/decode to/from manifest":
+  test "Should encode/decode to/from base manifest":
     var
       manifest = Manifest.new(
         treeCid = Cid.example,
         blockSize = 1.MiBs,
         datasetSize = 100.MiBs)
+
+    let
+      e = manifest.encode().tryGet()
+      decoded = Manifest.decode(e).tryGet()
+
+    check:
+      decoded == manifest
+
+  test "Should encode/decode to/from protected manifest":
+    var
+      manifest = Manifest.new(
+        manifest = Manifest.new(
+          treeCid = Cid.example,
+          blockSize = 1.MiBs,
+          datasetSize = 100.MiBs),
+        treeCid = Cid.example,
+        datasetSize = 200.MiBs,
+        eck = 10,
+        ecM = 10
+      )
 
     let
       e = manifest.encode().tryGet()
