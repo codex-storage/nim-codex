ba66709cf9c37681963bd9368c9e2a109816dd32
Eric <5089238+emizzle@users.noreply.github.com>
2023-12-14 15:27:24 +1100

    move serialization from rest/json to utils/json so it can be shared

--- codex/rest/json.nim
+++ codex/rest/json.nim
@@ -111,23 +111,4 @@ func `%`*(obj: StorageRequest | Slot): JsonNode =
 
   return jsonObj
 
-func `%`*(obj: Cid): JsonNode =
-  % $obj
-
-func `%`*(obj: PeerId): JsonNode =
-  % $obj
-
-func `%`*(obj: RestNodeId): JsonNode =
-  % $obj.id
-
-func `%`*(obj: SignedPeerRecord): JsonNode =
-  % $obj
-
-func `%`*(obj: dn.Address): JsonNode =
-  % $obj
-
-func `%`*(obj: AddressInfo): JsonNode =
-  % $obj.address
-
-func `%`*(obj: MultiAddress): JsonNode =
-  % $obj
+func `%`*(obj: RestNodeId): JsonNode = % $obj.id
