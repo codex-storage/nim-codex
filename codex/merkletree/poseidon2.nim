0fff11a8c2db5b0597c7436a9e5df671aa1af349
Dmitriy Ryajov <dryajov@gmail.com>
2023-12-16 22:54:29 -0600

    add fromNodes and converters

--- codex/merkletree/poseidon2.nim
+++ codex/merkletree/poseidon2.nim
@@ -11,7 +11,11 @@ import std/sequtils
 
 import pkg/poseidon2
 import pkg/constantine/math/io/io_fields
+import pkg/constantine/platforms/abstractions
 import pkg/questionable/results
+
+import ../utils
+import ../rng
 
 import ./merkletree
 
@@ -26,7 +30,7 @@ const
 type
   Poseidon2Hash* = F
 
-  PoseidonKeysEnum = enum  # can't use non-ordinals as enum values
+  PoseidonKeysEnum* = enum  # can't use non-ordinals as enum values
     KeyNone               = "0x0"
     KeyBottomLayer        = "0x1"
     KeyOdd                = "0x2"
@@ -35,14 +39,32 @@ type
   Poseidon2MerkleTree* = MerkleTree[Poseidon2Hash, PoseidonKeysEnum]
   Poseidon2MerkleProof* = MerkleProof[Poseidon2Hash, PoseidonKeysEnum]
 
-converter toKey(key: PoseidonKeysEnum): Poseidon2Hash =
+converter toKey*(key: PoseidonKeysEnum): Poseidon2Hash =
   case key:
   of KeyNone: KeyNoneF
   of KeyBottomLayer: KeyBottomLayerF
   of KeyOdd: KeyOddF
   of KeyOddAndBottomLayer: KeyOddAndBottomLayerF
 
-func init*(_: type Poseidon2MerkleTree, leaves: openArray[Poseidon2Hash]): ?!Poseidon2MerkleTree =
+converter toBool*(x: CtBool): bool =
+  bool(x)
+
+proc `==`*(a, b: Poseidon2MerkleTree): bool =
+  (a.leavesCount == b.leavesCount) and
+  (a.levels == b.levels)
+
+proc `==`*(a, b: Poseidon2MerkleProof): bool =
+  (a.nleaves == b.nleaves) and
+  (a.path == b.path) and
+  (a.index == b.index)
+
+func init*(
+  _: type Poseidon2MerkleTree,
+  leaves: openArray[Poseidon2Hash]): ?!Poseidon2MerkleTree =
+
+  if leaves.len == 0:
+    return failure "Empty leaves"
+
   let
     compressor = proc(
       x, y: Poseidon2Hash,
@@ -55,6 +77,40 @@ func init*(_: type Poseidon2MerkleTree, leaves: openArray[Poseidon2Hash]): ?!Pos
   self.layers = ? merkleTreeWorker(self, leaves, isBottomLayer = true)
   success self
 
-func init*(_: type Poseidon2MerkleTree, leaves: openArray[array[31, byte]]): ?!Poseidon2MerkleTree =
-  success Poseidon2MerkleProof.init(
-    leaves.mapIt( Poseidon2Hash.fromOpenArray(it) ))
+func init*(
+  _: type Poseidon2MerkleTree,
+  leaves: openArray[array[31, byte]]): ?!Poseidon2MerkleTree =
+  Poseidon2MerkleTree.init(
+    leaves.mapIt( Poseidon2Hash.fromBytes(it) ))
+
+proc fromNodes*(
+  _: type Poseidon2MerkleTree,
+  nodes: openArray[Poseidon2Hash],
+  nleaves: int): ?!Poseidon2MerkleTree =
+
+  if nodes.len == 0:
+    return failure "Empty nodes"
+
+  let
+    compressor = proc(
+      x, y: Poseidon2Hash,
+      key: PoseidonKeysEnum): ?!Poseidon2Hash {.noSideEffect.} =
+      success compress( x, y, key.toKey )
+
+  var
+    self = Poseidon2MerkleTree(compress: compressor, zero: zero)
+    layer = nleaves
+    pos = 0
+
+  while pos < nodes.len:
+    self.layers.add( nodes[pos..<(pos + layer)] )
+    pos += layer
+    layer = divUp(layer, 2)
+
+  let
+    index = Rng.instance.rand(nleaves - 1)
+    proof = ? self.getProof(index)
+
+  ? proof.verify(self.leaves[index], ? self.root) # sanity check
+
+  success self
