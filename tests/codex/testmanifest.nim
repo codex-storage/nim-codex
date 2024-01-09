9f578a4c76b2faf1a45f6c67f3e5e6888a93b9d1
Dmitriy Ryajov <dryajov@gmail.com>
2024-01-09 10:01:13 -0600

    fix manifest tests

--- tests/codex/testmanifest.nim
+++ tests/codex/testmanifest.nim
@@ -6,6 +6,10 @@ import pkg/asynctest
 import pkg/codex/chunker
 import pkg/codex/blocktype as bt
 import pkg/codex/manifest
+import pkg/poseidon2
+
+import pkg/codex/slots
+import pkg/codex/merkletree
 
 import ./helpers
 import ./examples
@@ -17,17 +21,30 @@ checksuite "Manifest":
       blockSize = 1.MiBs,
       datasetSize = 100.MiBs
     )
+
     protectedManifest = Manifest.new(
       manifest = manifest,
       treeCid = Cid.example,
       datasetSize = 200.MiBs,
-      eck = 10,
-      ecM = 10
+      eck = 2,
+      ecM = 2
     )
+
+    leaves = [
+      0.toF.Poseidon2Hash,
+      1.toF.Poseidon2Hash,
+      2.toF.Poseidon2Hash,
+      3.toF.Poseidon2Hash]
+
+    slotLeavesCids = leaves.toSlotCids().tryGet
+
+    tree = Poseidon2Tree.init(leaves).tryGet
+    slotsRootsCid = tree.root.tryGet.toSlotsRootsCid().tryGet
+
     verifiableManifest = Manifest.new(
       manifest = protectedManifest,
-      slotsRoot = Cid.example,
-      slotRoots = @[Cid.example, Cid.example]
+      slotsRoot = slotsRootsCid,
+      slotRoots = slotLeavesCids
     ).tryGet()
 
   proc encodeDecode(manifest: Manifest): Manifest =
