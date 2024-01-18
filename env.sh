d7e62bb0c203a8b6ee4e15702d26e68d91ef4d8f
gmega <giuliano.mega@gmail.com>
2024-01-18 17:34:52 -0300

    source env before running make, remove pending references to nim_version

--- env.sh
+++ env.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# This is the version that gets used if NIM_COMMIT is not specified.
+NIM_VERSION="v1.6.16"
+
 # We use ${BASH_SOURCE[0]} instead of $0 to allow sourcing this file
 # and we fall back to a Zsh-specific special var to also support Zsh.
 REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
 ABS_PATH="$(cd ${REL_PATH}; pwd)"
 
-# This determines the compiler version that gets used.
-export NIM_COMMIT="v1.6.16"
+export NIM_COMMIT="${NIM_COMMIT:-${NIM_VERSION}}"
 source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
