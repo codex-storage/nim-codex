8a35735b665da4ad4c82cdf5270c08a517210cbc
gmega <giuliano.mega@gmail.com>
2024-01-18 16:54:17 -0300

    drop nim version from CI and move it into env.sh

--- env.sh
+++ env.sh
@@ -4,4 +4,7 @@
 # and we fall back to a Zsh-specific special var to also support Zsh.
 REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
 ABS_PATH="$(cd ${REL_PATH}; pwd)"
+
+# This determines the compiler version that gets used.
+export NIM_COMMIT="v1.6.16"
 source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
