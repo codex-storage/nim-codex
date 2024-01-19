f69995f621891c28f1989f1f485ea954b8de02e8
gmega <giuliano.mega@gmail.com>
2024-01-19 19:03:57 -0300

    modify env.sh so it works with a uniform call interface for make

--- env.sh
+++ env.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-# This is the version that gets used if NIM_COMMIT is not specified.
-NIM_VERSION="v1.6.16"
+# This is the compiler version that will get used everywher.
+NIM_VERSION="v1.6.18"
 
 # We use ${BASH_SOURCE[0]} instead of $0 to allow sourcing this file
 # and we fall back to a Zsh-specific special var to also support Zsh.
 REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
 ABS_PATH="$(cd ${REL_PATH}; pwd)"
 
+ENV_FILE="${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh"
+
 export NIM_COMMIT="${NIM_COMMIT:-${NIM_VERSION}}"
-source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
+if ! [ -f "$ENV_FILE" ]; then
+  # Before the first "make update", the env file doesn't exist, so just run
+  # the command that comes after (without a child shell), if any. Running
+  # ./env.sh make update will cause the right compiler to be built right from
+  # the start.
+  echo "Nimbus env file not found"
+  "$@"
+else
+  source "${ENV_FILE}"
+fi
