fd787279787d3e7b20b1e0c4d56669aaf7df0147
gmega <giuliano.mega@gmail.com>
2024-01-19 10:45:03 -0300

    backport changes from https://github.com/codex-storage/nim-codex/pull/680 so I can use a custom compiler in dist tests without too much hassle

--- env.sh
+++ env.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+NIM_VERSION="f45bdea94ac4ed9a9bae03426275456aeb0cab2a"
+NIM_REPO_URL="https://github.com/gmega/Nim"
+
 # We use ${BASH_SOURCE[0]} instead of $0 to allow sourcing this file
 # and we fall back to a Zsh-specific special var to also support Zsh.
 REL_PATH="$(dirname ${BASH_SOURCE[0]:-${(%):-%x}})"
 ABS_PATH="$(cd ${REL_PATH}; pwd)"
+
+export NIM_COMMIT="${NIM_COMMIT:-${NIM_VERSION}}"
+export NIM_REPO="${NIM_REPO_URL:-${NIM_REPO_URL}}"
 source ${ABS_PATH}/vendor/nimbus-build-system/scripts/env.sh
