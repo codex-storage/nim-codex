a2052a2052815d5378b2c3c0031fa64237d90563
Dmitriy Ryajov <dryajov@gmail.com>
2024-01-22 14:55:44 -0600

    don't export types

--- codex/slots.nim
+++ codex/slots.nim
@@ -1,8 +1,9 @@
 import ./slots/builder
 import ./slots/sampler
+import ./slots/types
 import ./merkletree
 
-export builder, sampler
+export builder, sampler, types
 
 type
   Poseidon2Builder* = SlotsBuilder[Poseidon2Tree, Poseidon2Hash]
