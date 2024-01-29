856c2b9dc0ed61cac081766d4a693e1e383bbebf
Dmitriy Ryajov <dryajov@gmail.com>
2024-01-29 17:25:52 -0600

    wip merge master

--- codex/merkletree.nim
+++ codex/merkletree.nim
@@ -5,6 +5,6 @@ import ./merkletree/poseidon2
 export codex, poseidon2, merkletree
 
 type
-  SomeMerkleTree* = ByteTree | CodexTree | Poseidon2Tree
-  SomeMerkleProof* = ByteProof | CodexProof | Poseidon2Proof
-  SomeMerkleHash* = ByteHash | Poseidon2Hash
+  AnyMerkleTree* = ByteTree | CodexTree | Poseidon2Tree
+  AnyMerkleProof* = ByteProof | CodexProof | Poseidon2Proof
+  AnyMerkleHash* = ByteHash | Poseidon2Hash
