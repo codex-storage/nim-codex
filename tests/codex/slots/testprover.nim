d85b89a7c772b2376a00bc7b0cc3e683614fa26b
Dmitriy Ryajov <dryajov@gmail.com>
2024-02-06 12:36:42 -0600

    re-enable samper tests

--- tests/codex/slots/testprover.nim
+++ tests/codex/slots/testprover.nim
@@ -26,15 +26,6 @@ import ./helpers
 import ../helpers
 import ./backends/helpers
 
-const
-  slotId = 1
-  samples = 5
-  blockSize = DefaultBlockSize
-  cellSize = DefaultCellSize
-  ecK = 2
-  ecM = 2
-  numDatasetBlocks = 8
-
 suite "Test Prover":
   let
     slotId = 1
