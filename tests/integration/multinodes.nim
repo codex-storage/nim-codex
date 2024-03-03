c18cf7beef39f04c0b6230d2ec044efd11df7968
Mark Spanbroek <mark@spanbroek.net>
2024-03-03 06:47:03 +0100

    integration: do not start tests with a very outdated block

--- tests/integration/multinodes.nim
+++ tests/integration/multinodes.nim
@@ -266,6 +266,8 @@ template multinodesuite*(name: string, body: untyped) =
         # reverted in the test teardown
         if nodeConfigs.hardhat.isNone:
           snapshot = await send(ethProvider, "evm_snapshot")
+        # ensure that we have a recent block with a fresh timestamp
+        discard await send(ethProvider, "evm_mine")
         accounts = await ethProvider.listAccounts()
       except CatchableError as e:
         echo "Hardhat not running. Run hardhat manually " &
