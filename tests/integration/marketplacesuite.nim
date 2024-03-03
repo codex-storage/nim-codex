44ce3481a5c9f4e35f8faea23691bf2f47edfd63
Mark Spanbroek <mark@spanbroek.net>
2024-03-03 18:25:56 +0100

    integration: cleanup

--- tests/integration/marketplacesuite.nim
+++ tests/integration/marketplacesuite.nim
@@ -84,11 +84,7 @@ template marketplacesuite*(name: string, body: untyped) =
       return id
 
     setup:
-      # TODO: This is currently the address of the marketplace with a dummy
-      # verifier. Use real marketplace address, `Marketplace.address` once we
-      # can generate actual Groth16 ZK proofs.
-      let marketplaceAddress = Marketplace.address(dummyVerifier = false)
-      marketplace = Marketplace.new(marketplaceAddress, ethProvider.getSigner())
+      marketplace = Marketplace.new(Marketplace.address, ethProvider.getSigner())
       let tokenAddress = await marketplace.token()
       token = Erc20Token.new(tokenAddress, ethProvider.getSigner())
       let config = await mp.config(marketplace)
