94186abd0c950782c379c12d11c2867852a8d626
Ben <thatbenbierens@gmail.com>
2024-04-24 12:18:29 +0200

    cleanup

--- tests/codex/utils/testasyncdataevent.nim
+++ tests/codex/utils/testasyncdataevent.nim
@@ -5,36 +5,73 @@ import codex/utils/asyncdataevent
 import ../../asynctest
 import ../helpers
 
-asyncchecksuite "AsyncDataEvent":
-  test "Successful event":
-    let event = newAsyncDataEvent[int]()
+type
+  ExampleData = object
+    s: string
 
-    var data = 0
-    proc eventHandler(d: int): Future[?!void] {.async.} =
-      data = d
+asyncchecksuite "AsyncDataEvent":
+  var event: AsyncDataEvent[ExampleData]
+  let msg = "Yeah!"
+
+  setup:
+    event = newAsyncDataEvent[ExampleData]()
+
+  teardown:
+    await event.unsubscribeAll()
+
+  test "Successful event":
+    var data = ""
+    proc eventHandler(e: ExampleData): Future[?!void] {.async.} =
+      data = e.s
       success()
 
-    let handle = event.subscribeA(eventHandler)
+    event.subscribe(eventHandler)
 
     check:
-      isOK(await event.fireA(123))
-      data == 123
-
-    await event.unsubscribeA(handle)
+      isOK(await event.fire(ExampleData(
+        s: msg
+      )))
+      data == msg
 
   test "Failed event preserves error message":
-    let
-      event = newAsyncDataEvent[int]()
-      msg = "Error message!"
-
-    proc eventHandler(d: int): Future[?!void] {.async.} =
+    proc eventHandler(e: ExampleData): Future[?!void] {.async.} =
       failure(msg)
 
-    let handle = event.subscribeA(eventHandler)
-    let fireResult = await event.fireA(123)
+    event.subscribe(eventHandler)
+    let fireResult = await event.fire(ExampleData(
+      s: "a"
+    ))
 
     check:
       fireResult.isErr
       fireResult.error.msg == msg
 
-    await event.unsubscribeA(handle)
+  test "Emits data to multiple subscribers":
+    var
+      data1 = ""
+      data2 = ""
+      data3 = ""
+
+    proc handler1(e: ExampleData): Future[?!void] {.async.} =
+      data1 = e.s
+      success()
+    proc handler2(e: ExampleData): Future[?!void] {.async.} =
+      data2 = e.s
+      success()
+    proc handler3(e: ExampleData): Future[?!void] {.async.} =
+      data3 = e.s
+      success()
+
+    event.subscribe(handler1)
+    event.subscribe(handler2)
+    event.subscribe(handler3)
+
+    let fireResult = await event.fire(ExampleData(
+        s: msg
+    ))
+
+    check:
+      fireResult.isOK
+      data1 == msg
+      data2 == msg
+      data3 == msg
