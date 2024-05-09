4d290d92f3f77e3d53545097912d513d414fab69
Adam Uhlíř <adam@uhlir.dev>
2024-05-09 09:34:26 +0200

    fix: correct place to plug confirm

--- codex/purchasing/states/pending.nim
+++ codex/purchasing/states/pending.nim
@@ -14,5 +14,5 @@ method run*(state: PurchasePending, machine: Machine): Future[?State] {.async.} 
   codex_purchases_pending.inc()
   let purchase = Purchase(machine)
   let request = !purchase.request
-  await purchase.market.requestStorage(request).confirm(1)
+  await purchase.market.requestStorage(request)
   return some State(PurchaseSubmitted())
