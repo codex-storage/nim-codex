a0a43f1f208d3b7b8176165ad7508ce7eff2fbc4
Marcin Czenko <marcin.czenko@pm.me>
2024-10-15 00:58:56 +0200

    adds validation to the set of integration tests

--- tests/testIntegration.nim
+++ tests/testIntegration.nim
@@ -6,6 +6,7 @@ import ./integration/testpurchasing
 import ./integration/testblockexpiration
 import ./integration/testmarketplace
 import ./integration/testproofs
+import ./integration/testvalidator
 import ./integration/testecbug
 
 {.warning[UnusedImport]:off.}
