ad0b8b6862f6607db1caeda88bec84680d31d72e
Marcin Czenko <marcin.czenko@pm.me>
2024-10-17 06:42:13 +0200

    Makes validator integration test more robust - adds extra tracking

--- tests/testIntegration.nim
+++ tests/testIntegration.nim
@@ -1,12 +1,12 @@
-import ./integration/testcli
-import ./integration/testrestapi
-import ./integration/testupdownload
-import ./integration/testsales
-import ./integration/testpurchasing
-import ./integration/testblockexpiration
-import ./integration/testmarketplace
-import ./integration/testproofs
+# import ./integration/testcli
+# import ./integration/testrestapi
+# import ./integration/testupdownload
+# import ./integration/testsales
+# import ./integration/testpurchasing
+# import ./integration/testblockexpiration
+# import ./integration/testmarketplace
+# import ./integration/testproofs
 import ./integration/testvalidator
-import ./integration/testecbug
+# import ./integration/testecbug
 
 {.warning[UnusedImport]:off.}
