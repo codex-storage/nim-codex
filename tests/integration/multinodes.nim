e6e0db124dd57adee2e6f5ab62ee5e30d9954a66
Marcin Czenko <marcin.czenko@pm.me>
2024-11-27 04:49:20 +0100

    makes sure that clients, SPs, and validators use the same provider url

--- tests/integration/multinodes.nim
+++ tests/integration/multinodes.nim
@@ -224,6 +224,7 @@ template multinodesuiteWithProviderUrl*(name: string, jsonRpcProviderUrl: string
     proc startClientNode(conf: CodexConfig): Future[NodeProcess] {.async.} =
       let clientIdx = clients().len
       var config = conf
+      config.addCliOption(StartUpCmd.persistence, "--eth-provider", jsonRpcProviderUrl)
       config.addCliOption(StartUpCmd.persistence, "--eth-account", $accounts[running.len])
       return await newCodexProcess(clientIdx, config, Role.Client)
 
@@ -231,6 +232,7 @@ template multinodesuiteWithProviderUrl*(name: string, jsonRpcProviderUrl: string
       let providerIdx = providers().len
       var config = conf
       config.addCliOption("--bootstrap-node", bootstrap)
+      config.addCliOption(StartUpCmd.persistence, "--eth-provider", jsonRpcProviderUrl)
       config.addCliOption(StartUpCmd.persistence, "--eth-account", $accounts[running.len])
       config.addCliOption(PersistenceCmd.prover, "--circom-r1cs",
         "vendor/codex-contracts-eth/verifier/networks/hardhat/proof_main.r1cs")
@@ -245,6 +247,7 @@ template multinodesuiteWithProviderUrl*(name: string, jsonRpcProviderUrl: string
       let validatorIdx = validators().len
       var config = conf
       config.addCliOption("--bootstrap-node", bootstrap)
+      config.addCliOption(StartUpCmd.persistence, "--eth-provider", jsonRpcProviderUrl)
       config.addCliOption(StartUpCmd.persistence, "--eth-account", $accounts[running.len])
       config.addCliOption(StartUpCmd.persistence, "--validator")
 
