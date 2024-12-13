217b588cedf2c8f526343e35d413b1b57512e235
Marcin Czenko <marcin.czenko@pm.me>
2024-12-13 23:04:54 +0100

    adds testProvider to testContracts group

--- tests/testContracts.nim
+++ tests/testContracts.nim
@@ -2,5 +2,6 @@ import ./contracts/testContracts
 import ./contracts/testMarket
 import ./contracts/testDeployment
 import ./contracts/testClock
+import ./contracts/testProvider
 
 {.warning[UnusedImport]:off.}
