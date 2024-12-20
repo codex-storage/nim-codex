93d3de315e15dee9cbeaadc28e8bdb5e7667f356
Eric <5089238+emizzle@users.noreply.github.com>
2024-12-20 16:23:40 +1100

    parallel tests running
    - need to test with longer tests to ensure the parallelisation is truly happening
    - is the +10 hardhat port needed?
    - try with more integration tests
    
    # Conflicts:
    #	tests/integration/hardhatprocess.nim
    #	tests/integration/multinodes.nim
    #	tests/integration/testcli.nim
    #	tests/testIntegration.nim

---
+++ tests/integration/utils.nim
@@ -0,0 +1,25 @@
+import pkg/chronos
+import pkg/codex/logutils
+
+proc nextFreePort*(startPort: int): Future[int] {.async: (raises:[CancelledError]).} =
+
+  proc client(server: StreamServer, transp: StreamTransport) {.async.} =
+    await transp.closeWait()
+
+  var port = startPort
+  while true:
+    trace "checking if port is free", port
+    try:
+      let host = initTAddress("127.0.0.1", port)
+      # We use ReuseAddr here only to be able to reuse the same IP/Port when
+      # there's a TIME_WAIT socket. It's useful when running the test multiple
+      # times or if a test ran previously using the same port.
+      var server = createStreamServer(host, client, {ReuseAddr})
+      trace "port is free", port
+      await server.closeWait()
+      return port
+    except TransportOsError:
+      trace "port is not free", port
+      inc port
+    except TransportAddressError:
+      raiseAssert "bad address"
