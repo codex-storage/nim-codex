b588e00a6936194d64e91cf39a0577cb82a67034
Adam Uhlíř <adam@uhlir.dev>
2025-01-07 09:04:05 +0100

    refactor: marketplace custom errors handling

--- codex/contracts/market.nim
+++ codex/contracts/market.nim
@@ -485,7 +485,8 @@ method queryPastSlotFilledEvents*(
     market: OnChainMarket, fromTime: SecondsSince1970
 ): Future[seq[SlotFilled]] {.async.} =
   convertEthersError:
-    let fromBlock = await market.contract.provider.blockNumberForEpoch(fromTime)
+    let fromBlock =
+      await market.contract.provider.blockNumberForEpoch(fromTime)
     return await market.queryPastSlotFilledEvents(BlockTag.init(fromBlock))
 
 method queryPastStorageRequestedEvents*(
