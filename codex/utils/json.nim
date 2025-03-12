c8bd150cd69469a30a40382cd54cef68d18dd042
Marcin Czenko <marcin.czenko@pm.me>
2025-03-12 02:59:02 +0100

    adds JSON serialization to BitTorrent manifest

--- codex/utils/json.nim
+++ codex/utils/json.nim
@@ -2,7 +2,8 @@ import std/options
 import std/typetraits
 from pkg/ethers import Address
 from pkg/libp2p import
-  Cid, PeerId, SignedPeerRecord, MultiAddress, AddressInfo, init, `$`
+  Cid, PeerId, SignedPeerRecord, MultiAddress, AddressInfo, MultiHash, init, hex, `$`
+import pkg/stew/byteutils
 import pkg/contractabi
 import pkg/codexdht/discv5/node as dn
 import pkg/serde/json
@@ -35,3 +36,13 @@ func `%`*(obj: MultiAddress): JsonNode =
 
 func `%`*(address: ethers.Address): JsonNode =
   % $address
+
+proc fromJson*(_: type MultiHash, json: JsonNode): ?!MultiHash =
+  expectJsonKind(MultiHash, JString, json)
+  echo "[MultiHash.fromJson] json.str: ", json.str
+  without bytes =? json.str.hexToSeqByte.catch, err:
+    return failure(err.msg)
+  MultiHash.init(bytes).mapFailure
+
+func `%`*(multiHash: MultiHash): JsonNode =
+  %multiHash.hex
