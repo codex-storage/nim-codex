e9709892050de45ef541514dcfd2a7317e979bb6
Eric <5089238+emizzle@users.noreply.github.com>
2025-03-24 17:20:59 +1100

    Reinstate unittest > unittest2, move eventually to ./asynctest

--- tests/asynctest.nim
+++ tests/asynctest.nim
@@ -1,3 +1,13 @@
-import pkg/asynctest/chronos/unittest
+import pkg/asynctest/chronos/unittest2
 
-export unittest
+export unittest2 except eventually
+
+template eventually*(expression: untyped, timeout = 5000, pollInterval = 10): bool =
+  ## Fast defaults, do not use with HTTP connections!
+  eventually(expression, timeout, pollInterval)
+
+template eventuallySafe*(
+    expression: untyped, timeout = 5000, pollInterval = 1000
+): bool =
+  ## More sane defaults, for use with HTTP connections
+  eventually(expression, timeout = timeout, pollInterval = pollInterval)
