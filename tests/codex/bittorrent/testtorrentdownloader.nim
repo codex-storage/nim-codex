5a65c1af60706953031dbf4a5b02a2339a86eea4
Marcin Czenko <marcin.czenko@pm.me>
2025-03-27 16:13:24 +0100

    updates copyright

--- tests/codex/bittorrent/testtorrentdownloader.nim
+++ tests/codex/bittorrent/testtorrentdownloader.nim
@@ -1,12 +1,3 @@
-## Nim-Codex
-## Copyright (c) 2025 Status Research & Development GmbH
-## Licensed under either of
-##  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
-##  * MIT license ([LICENSE-MIT](LICENSE-MIT))
-## at your option.
-## This file may not be copied, modified, or distributed except according to
-## those terms.
-
 import std/importutils # private access
 
 import pkg/libp2p/[cid, multicodec, multihash]
