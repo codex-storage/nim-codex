f4cbf48e44030d8521ab898d38180f668f9a7c57
Marcin Czenko <marcin.czenko@pm.me>
2025-03-27 16:10:12 +0100

    updates and integrates torrentdownloader into api and node

--- codex/bittorrent/torrentdownloader.nim
+++ codex/bittorrent/torrentdownloader.nim
@@ -1,13 +1,20 @@
+## Nim-Codex
+## Copyright (c) 2025 Status Research & Development GmbH
+## Licensed under either of
+##  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+##  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+## at your option.
+## This file may not be copied, modified, or distributed except according to
+## those terms.
+
 {.push raises: [].}
 
-# import std/asyncstreams
 import std/sequtils
 import std/sugar
 import pkg/chronos
 import pkg/libp2p/multihash
 import pkg/questionable/results
 
-# import ../rng
 import ../logutils
 import ../utils/iter
 import ../utils/trackedfutures
@@ -42,111 +49,11 @@ type
     queue: AsyncQueue[TorrentPiece]
     trackedFutures: TrackedFutures
 
-proc newTorrentPiece*(
-    pieceIndex: int, pieceHash: MultiHash, blockIndexStart: int, blockIndexEnd: int
-): TorrentPiece =
-  TorrentPiece(
-    pieceIndex: pieceIndex,
-    pieceHash: pieceHash,
-    blockIndexStart: blockIndexStart,
-    blockIndexEnd: blockIndexEnd,
-    handle: cast[PieceHandle](newFuture[void]("PieceValidator.newTorrentPiece")),
-  )
-
-proc newTorrentDownloader*(
-    torrentManifest: BitTorrentManifest,
-    codexManifest: Manifest,
-    networkStore: NetworkStore,
-): ?!TorrentDownloader =
-  let
-    blocksCount = codexManifest.blocksCount
-    numOfPieces = torrentManifest.info.pieces.len
-    numOfBlocksPerPiece =
-      torrentManifest.info.pieceLength.int div codexManifest.blockSize.int
-    numOfBlocksInLastPiece = blocksCount - (numOfBlocksPerPiece * (numOfPieces - 1))
-
-  let pieces = collect:
-    for i in 0 ..< numOfPieces:
-      var blockIndexEnd = ((i + 1) * numOfBlocksPerPiece) - 1
-      if i == numOfPieces - 1:
-        # last piece can have less blocks than numOfBlocksPerPiece
-        blockIndexEnd = i * numOfBlocksPerPiece + numOfBlocksInLastPiece - 1
-
-      let piece = newTorrentPiece(
-        pieceIndex = i,
-        pieceHash = torrentManifest.info.pieces[i],
-        blockIndexStart = i * numOfBlocksPerPiece,
-        blockIndexEnd = blockIndexEnd,
-      )
-      piece
-
-  let queue = newAsyncQueue[TorrentPiece](maxsize = numOfPieces)
-
-  let iter = Iter.new(0 ..< numOfPieces)
-  var pieceDownloadSequence = newSeqWith(numOfPieces, iter.next())
-  # optional: randomize the order of pieces
-  # not sure if this is such a great idea when streaming content
-  # Rng.instance.shuffle(pieceDownloadSequence)
-
-  trace "Piece download sequence", pieceDownloadSequence
-
-  for i in pieceDownloadSequence:
-    try:
-      queue.addLastNoWait(pieces[i])
-    except AsyncQueueFullError:
-      raiseAssert "Fatal: could not add pieces to queue"
-
-  TorrentDownloader(
-    torrentManifest: torrentManifest,
-    codexManifest: codexManifest,
-    networkStore: networkStore,
-    numberOfPieces: numOfPieces,
-    numberOfBlocksPerPiece: numOfBlocksPerPiece,
-    pieces: pieces,
-    waitIter: Iter[int].new(0 ..< numOfPieces),
-    blockIter: Iter[int].empty(),
-    pieceIndex: 0,
-    queue: queue,
-    trackedFutures: TrackedFutures(),
-  ).success
-
-proc getNewBlockIterator(piece: TorrentPiece): Iter[int] =
-  Iter[int].new(piece.blockIndexStart .. piece.blockIndexEnd)
-
 func numberOfBlocks(piece: TorrentPiece): int =
   piece.blockIndexEnd - piece.blockIndexStart + 1
 
-func numberOfBlocksInPiece*(self: TorrentDownloader, pieceIndex: int): ?!int =
-  if pieceIndex < 0 or pieceIndex >= self.numberOfPieces:
-    return failure("Invalid piece index")
-  let piece = self.pieces[pieceIndex]
-  success(piece.numberOfBlocks)
-
-proc getNewBlocksInPieceIterator*(
-    self: TorrentDownloader, pieceIndex: int
-): ?!Iter[int] =
-  if pieceIndex < 0 or pieceIndex >= self.numberOfPieces:
-    return failure("Invalid piece index")
-  let piece = self.pieces[pieceIndex]
-  success(piece.getNewBlockIterator())
-
-proc getNewPieceIterator*(self: TorrentDownloader): Iter[int] =
-  Iter[int].new(0 ..< self.numberOfPieces)
-
-# proc getNewBlocksPerPieceIterator*(self: TorrentDownloader): Iter[int] =
-#   Iter[int].new(0 ..< self.numberOfBlocksPerPiece)
-
-proc waitForNextPiece*(
-    self: TorrentDownloader
-): Future[int] {.async: (raises: [CancelledError]).} =
-  if self.waitIter.finished:
-    return -1
-  let pieceIndex = self.waitIter.next()
-  await self.pieces[pieceIndex].handle
-  pieceIndex
-
-proc cancel*(self: TorrentDownloader): Future[void] {.async: (raises: []).} =
-  await noCancel allFutures(self.pieces.mapIt(it.handle.cancelAndWait))
+proc getNewBlockIterator(piece: TorrentPiece): Iter[int] =
+  Iter[int].new(piece.blockIndexStart .. piece.blockIndexEnd)
 
 proc validate(piece: TorrentPiece, blocks: seq[Block]): ?!void {.raises: [].} =
   var pieceHashCtx: sha1
@@ -223,6 +130,37 @@ proc fetchPiece(
 
   success()
 
+func numberOfBlocksInPiece*(self: TorrentDownloader, pieceIndex: int): ?!int =
+  if pieceIndex < 0 or pieceIndex >= self.numberOfPieces:
+    return failure("Invalid piece index")
+  let piece = self.pieces[pieceIndex]
+  success(piece.numberOfBlocks)
+
+###########################################################################
+# Public API
+###########################################################################
+
+proc getNewBlockIterator*(self: TorrentDownloader, pieceIndex: int): ?!Iter[int] =
+  if pieceIndex < 0 or pieceIndex >= self.numberOfPieces:
+    return failure("Invalid piece index")
+  let piece = self.pieces[pieceIndex]
+  success(piece.getNewBlockIterator())
+
+proc getNewPieceIterator*(self: TorrentDownloader): Iter[int] =
+  Iter[int].new(0 ..< self.numberOfPieces)
+
+proc waitForNextPiece*(
+    self: TorrentDownloader
+): Future[int] {.async: (raises: [CancelledError]).} =
+  if self.waitIter.finished:
+    return -1
+  let pieceIndex = self.waitIter.next()
+  await self.pieces[pieceIndex].handle
+  pieceIndex
+
+proc cancel*(self: TorrentDownloader): Future[void] {.async: (raises: []).} =
+  await noCancel allFutures(self.pieces.mapIt(it.handle.cancelAndWait))
+
 proc downloadPieces*(self: TorrentDownloader): Future[void] {.async: (raises: []).} =
   try:
     while not self.queue.empty:
@@ -249,33 +187,6 @@ proc downloadPieces*(self: TorrentDownloader): Future[void] {.async: (raises: []
   finally:
     await noCancel self.cancel()
 
-# proc downloadPieces*(self: TorrentDownloader): Future[?!void] {.async: (raises: []).} =
-#   try:
-#     while not self.queue.empty:
-#       let piece = self.queue.popFirstNoWait()
-#       if err =? (await self.fetchPiece(piece)).errorOption:
-#         error "Could not fetch piece", err = err.msg
-#         # add the piece to the end of the queue
-#         # to try to fetch the piece again
-#         self.queue.addLastNoWait(piece)
-#         continue
-#       else:
-#         # piece fetched and validated successfully
-#         # mark it as ready
-#         piece.handle.complete()
-#       await sleepAsync(1.millis)
-#   except CancelledError:
-#     trace "Downloading pieces cancelled"
-#   except AsyncQueueFullError as e:
-#     error "Queue is full", error = e.msg
-#     return failure e
-#   except AsyncQueueEmptyError as e:
-#     error "Trying to pop from empty queue", error = e.msg
-#     return failure e
-#   finally:
-#     await noCancel self.cancel()
-#   success()
-
 proc getNext*(
     self: TorrentDownloader
 ): Future[?!(int, seq[byte])] {.async: (raises: []).} =
@@ -283,7 +194,7 @@ proc getNext*(
     if self.pieceIndex == -1:
       return success((-1, newSeq[byte]()))
     if self.blockIter.finished:
-      trace "Waiting for piece", pieceIndex = self.pieceIndex
+      trace "Waiting for piece to be ready"
       self.pieceIndex = await self.waitForNextPiece()
       trace "Got piece", pieceIndex = self.pieceIndex
       if self.pieceIndex == -1:
@@ -317,29 +228,70 @@ proc stop*(self: TorrentDownloader) {.async.} =
   await noCancel self.cancel()
   await noCancel self.trackedFutures.cancelTracked()
 
-#################################################################
-# Previous API, keeping it for now, probably will not be needed
-#
-#################################################################
+proc newTorrentPiece*(
+    pieceIndex: int, pieceHash: MultiHash, blockIndexStart: int, blockIndexEnd: int
+): TorrentPiece =
+  TorrentPiece(
+    pieceIndex: pieceIndex,
+    pieceHash: pieceHash,
+    blockIndexStart: blockIndexStart,
+    blockIndexEnd: blockIndexEnd,
+    handle: cast[PieceHandle](newFuture[void]("PieceValidator.newTorrentPiece")),
+  )
 
-proc waitForPiece*(
-    self: TorrentDownloader, index: int
-): Future[?!void] {.async: (raises: [CancelledError]).} =
-  if index < 0 or index >= self.pieces.len:
-    return failure("Invalid piece index")
-  await self.pieces[index].handle
-  success()
+proc newTorrentDownloader*(
+    torrentManifest: BitTorrentManifest,
+    codexManifest: Manifest,
+    networkStore: NetworkStore,
+): ?!TorrentDownloader =
+  let
+    blocksCount = codexManifest.blocksCount
+    numOfPieces = torrentManifest.info.pieces.len
+    numOfBlocksPerPiece =
+      torrentManifest.info.pieceLength.int div codexManifest.blockSize.int
+    numOfBlocksInLastPiece = blocksCount - (numOfBlocksPerPiece * (numOfPieces - 1))
 
-proc cancelPiece*(
-    self: TorrentDownloader, index: int
-): Future[?!void] {.async: (raises: [CancelledError]).} =
-  if index < 0 or index >= self.pieces.len:
-    return failure("Invalid piece index")
-  await noCancel self.pieces[index].handle.cancelAndWait()
-  success()
+  let pieces = collect:
+    for i in 0 ..< numOfPieces:
+      var blockIndexEnd = ((i + 1) * numOfBlocksPerPiece) - 1
+      if i == numOfPieces - 1:
+        # last piece can have less blocks than numOfBlocksPerPiece
+        blockIndexEnd = i * numOfBlocksPerPiece + numOfBlocksInLastPiece - 1
 
-proc confirmPiece*(self: TorrentDownloader, index: int): ?!void {.raises: [].} =
-  if index < 0 or index >= self.pieces.len:
-    return failure("Invalid piece index")
-  self.pieces[index].handle.complete()
-  success()
+      let piece = newTorrentPiece(
+        pieceIndex = i,
+        pieceHash = torrentManifest.info.pieces[i],
+        blockIndexStart = i * numOfBlocksPerPiece,
+        blockIndexEnd = blockIndexEnd,
+      )
+      piece
+
+  let queue = newAsyncQueue[TorrentPiece](maxsize = numOfPieces)
+
+  let iter = Iter.new(0 ..< numOfPieces)
+  var pieceDownloadSequence = newSeqWith(numOfPieces, iter.next())
+  # optional: randomize the order of pieces
+  # not sure if this is such a great idea when streaming content
+  # Rng.instance.shuffle(pieceDownloadSequence)
+
+  trace "Piece download sequence", pieceDownloadSequence
+
+  for i in pieceDownloadSequence:
+    try:
+      queue.addLastNoWait(pieces[i])
+    except AsyncQueueFullError:
+      raiseAssert "Fatal: could not add pieces to queue"
+
+  TorrentDownloader(
+    torrentManifest: torrentManifest,
+    codexManifest: codexManifest,
+    networkStore: networkStore,
+    numberOfPieces: numOfPieces,
+    numberOfBlocksPerPiece: numOfBlocksPerPiece,
+    pieces: pieces,
+    waitIter: Iter[int].new(0 ..< numOfPieces),
+    blockIter: Iter[int].empty(),
+    pieceIndex: 0,
+    queue: queue,
+    trackedFutures: TrackedFutures(),
+  ).success
