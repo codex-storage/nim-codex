9a688ba56620b03307c27910e5892ef0d53afa9a
Marcin Czenko <marcin.czenko@pm.me>
2025-05-28 16:21:51 +0200

    adds magent link and torrent parser tests to bittorrent tests

--- tests/codex/testbittorrent.nim
+++ tests/codex/testbittorrent.nim
@@ -2,5 +2,7 @@ import ./bittorrent/testbencoding
 import ./bittorrent/testmanifest
 import ./bittorrent/testpiecevalidator
 import ./bittorrent/testtorrentdownloader
+import ./bittorrent/testmagnetlink
+import ./bittorrent/testtorrentparser
 
 {.warning[UnusedImport]: off.}
