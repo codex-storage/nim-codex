9cf154c55438712ffe47eb770f92c774669d0e9c
Marcin Czenko <marcin.czenko@pm.me>
2025-06-16 18:49:48 +0200

    removes last references to legacy asynciter in tests

--- tests/codex/testutils.nim
+++ tests/codex/testutils.nim
@@ -1,7 +1,6 @@
 import ./utils/testoptions
 import ./utils/testkeyutils
 import ./utils/testasyncstatemachine
-import ./utils/testasynciter
 import ./utils/testsafeasynciter
 import ./utils/testtimer
 import ./utils/testtrackedfutures
