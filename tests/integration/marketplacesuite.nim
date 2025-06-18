e6da066ec86af733298b6a9b3ca5e13dadd2cd7b
Arnaud <arnaud@status.im>
2025-06-18 10:18:17 +0200

    Remove useless default value for stopOnRequestFail

--- tests/integration/marketplacesuite.nim
+++ tests/integration/marketplacesuite.nim
@@ -12,7 +12,7 @@ import ../contracts/deployment
 export mp
 export multinodes
 
-template marketplacesuite*(name: string, stopOnRequestFail = true, body: untyped) =
+template marketplacesuite*(name: string, stopOnRequestFail: bool, body: untyped) =
   multinodesuite name:
     var marketplace {.inject, used.}: Marketplace
     var period: uint64
