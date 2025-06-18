e0233e614f08f447df38c6620517b7a51c131365
Arnaud <arnaud@status.im>
2025-06-18 16:56:05 +0200

    Apply markplace test changes and use ERC20 subscriptions

--- tests/integration/marketplacesuite.nim
+++ tests/integration/marketplacesuite.nim
@@ -5,22 +5,48 @@ import pkg/codex/contracts/marketplace as mp
 import pkg/codex/periods
 import pkg/codex/utils/json
 from pkg/codex/utils import roundUp, divUp
-import ./multinodes
+import ./multinodes except Subscription
 import ../contracts/time
 import ../contracts/deployment
 
 export mp
 export multinodes
 
-template marketplacesuite*(name: string, body: untyped) =
+template marketplacesuite*(name: string, stopOnRequestFail: bool, body: untyped) =
   multinodesuite name:
     var marketplace {.inject, used.}: Marketplace
     var period: uint64
     var periodicity: Periodicity
     var token {.inject, used.}: Erc20Token
+    var requestStartedEvent: AsyncEvent
+    var requestStartedSubscription: Subscription
+    var requestFailedEvent: AsyncEvent
+    var requestFailedSubscription: Subscription
+
+    proc onRequestStarted(eventResult: ?!RequestFulfilled) {.raises: [].} =
+      requestStartedEvent.fire()
+
+    proc onRequestFailed(eventResult: ?!RequestFailed) {.raises: [].} =
+      requestFailedEvent.fire()
+      if stopOnRequestFail:
+        fail()
 
     proc getCurrentPeriod(): Future[Period] {.async.} =
       return periodicity.periodOf((await ethProvider.currentTime()).truncate(uint64))
+
+    proc waitForRequestToStart(
+        seconds = 10 * 60 + 10
+    ): Future[Period] {.async: (raises: [CancelledError, AsyncTimeoutError]).} =
+      await requestStartedEvent.wait().wait(timeout = chronos.seconds(seconds))
+      # Recreate a new future if we need to wait for another request
+      requestStartedEvent = newAsyncEvent()
+
+    proc waitForRequestToFail(
+        seconds = (5 * 60) + 10
+    ): Future[Period] {.async: (raises: [CancelledError, AsyncTimeoutError]).} =
+      await requestFailedEvent.wait().wait(timeout = chronos.seconds(seconds))
+      # Recreate a new future if we need to wait for another request
+      requestFailedEvent = newAsyncEvent()
 
     proc advanceToNextPeriod() {.async.} =
       let periodicity = Periodicity(seconds: period)
@@ -30,7 +56,7 @@ template marketplacesuite*(name: string, body: untyped) =
       await ethProvider.advanceTimeTo(endOfPeriod.u256 + 1)
 
     template eventuallyP(condition: untyped, finalPeriod: Period): bool =
-      proc eventuallyP(): Future[bool] {.async.} =
+      proc eventuallyP(): Future[bool] {.async: (raises: [CancelledError]).} =
         while (
           let currentPeriod = await getCurrentPeriod()
           currentPeriod <= finalPeriod
@@ -109,5 +135,18 @@ template marketplacesuite*(name: string, body: untyped) =
       let config = await marketplace.configuration()
       period = config.proofs.period
       periodicity = Periodicity(seconds: period)
+
+      requestStartedEvent = newAsyncEvent()
+      requestFailedEvent = newAsyncEvent()
+
+      requestStartedSubscription =
+        await marketplace.subscribe(RequestFulfilled, onRequestStarted)
+
+      requestFailedSubscription =
+        await marketplace.subscribe(RequestFailed, onRequestFailed)
+
+    teardown:
+      await requestStartedSubscription.unsubscribe()
+      await requestFailedSubscription.unsubscribe()
 
     body
