c54e3b8dad37b2d06b38a6a04fea63193029534f
Mark Spanbroek <mark@spanbroek.net>
2025-06-23 15:21:34 +0200

    refactor(tests): multiple ci jobs for integration tests
    Each integration test job takes at most 30 minutes to run

--- .github/workflows/ci-job-matrix.sh
+++ .github/workflows/ci-job-matrix.sh
@@ -1,15 +1,127 @@
 #!/usr/bin/env bash
-echo "["
-echo "{\"os\": \"linux\",   \"cpu\": \"amd64\", \"builder\": \"ubuntu-latest\",  \"tests\": \"unittest\",    \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"linux\",   \"cpu\": \"amd64\", \"builder\": \"ubuntu-latest\",  \"tests\": \"contract\",    \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"linux\",   \"cpu\": \"amd64\", \"builder\": \"ubuntu-latest\",  \"tests\": \"integration\", \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"linux\",   \"cpu\": \"amd64\", \"builder\": \"ubuntu-latest\",  \"tests\": \"tools\",       \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"macos\",   \"cpu\": \"arm64\", \"builder\": \"macos-14\",       \"tests\": \"unittest\",    \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"macos\",   \"cpu\": \"arm64\", \"builder\": \"macos-14\",       \"tests\": \"contract\",    \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"macos\",   \"cpu\": \"arm64\", \"builder\": \"macos-14\",       \"tests\": \"integration\", \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"macos\",   \"cpu\": \"arm64\", \"builder\": \"macos-14\",       \"tests\": \"tools\",       \"nim_version\": \"${nim_version}\", \"shell\": \"bash --noprofile --norc -e -o pipefail\"},"
-echo "{\"os\": \"windows\", \"cpu\": \"amd64\", \"builder\": \"windows-latest\", \"tests\": \"unittest\",    \"nim_version\": \"${nim_version}\", \"shell\": \"msys2\"},"
-echo "{\"os\": \"windows\", \"cpu\": \"amd64\", \"builder\": \"windows-latest\", \"tests\": \"contract\",    \"nim_version\": \"${nim_version}\", \"shell\": \"msys2\"},"
-echo "{\"os\": \"windows\", \"cpu\": \"amd64\", \"builder\": \"windows-latest\", \"tests\": \"integration\", \"nim_version\": \"${nim_version}\", \"shell\": \"msys2\"},"
-echo "{\"os\": \"windows\", \"cpu\": \"amd64\", \"builder\": \"windows-latest\", \"tests\": \"tools\",       \"nim_version\": \"${nim_version}\", \"shell\": \"msys2\"}"
-echo "]"
+
+# remembers how many jobs were generated
+jobs_generated=0
+
+# outputs a github actions job
+job () {
+  # output a comma separator between jobs
+  if (( $jobs_generated >= 1 )); then
+    echo -n ","
+  fi
+  (( jobs_generated++ ))
+
+  # output github actions job as JSON
+  echo "{\
+    \"os\": \"${job_os}\", \
+    \"cpu\": \"${job_cpu}\", \
+    \"builder\": \"${job_builder}\", \
+    \"tests\": \"${job_tests}\", \
+    \"includes\": \"${job_includes}\", \
+    \"nim_version\": \"${nim_version}\", \
+    \"shell\": \"${job_shell}\"\
+  }"
+}
+
+# sets parameters for a linux job
+linux () {
+  job_os="linux"
+  job_cpu="amd64"
+  job_builder="ubuntu-latest"
+  job_shell="bash --noprofile --norc -e -o pipefail"
+}
+
+# sets parameters for a macos job
+macos () {
+  job_os="macos"
+  job_cpu="arm64"
+  job_builder="macos-14"
+  job_shell="bash --noprofile --norc -e -o pipefail"
+}
+
+# sets parameters for a windows job
+windows () {
+  job_os="windows"
+  job_cpu="amd64"
+  job_builder="windows-latest"
+  job_shell="msys2"
+}
+
+# outputs a unit test job
+unit_test () {
+  job_tests="unittest"
+  job_includes=""
+  job
+}
+
+# outputs a contract test job
+contract_test () {
+  job_tests="contract"
+  job_includes=""
+  job
+}
+
+# outputs a tools test job
+tools_test () {
+  job_tests="tools"
+  job_includes=""
+  job
+}
+
+# finds all files named test*.nim in the specified directory
+find_tests () {
+  local dir=$1
+  find $dir -name 'test*.nim'
+}
+
+# creates batches from stdin elements, joined by a separator
+batch () {
+  local batch_size=$1
+  local separator=$2
+  xargs -n $batch_size bash -c "IFS=\"$separator\"; echo \"\$*\"" _
+}
+
+# outputs a single integration test job
+integration_test_job () {
+  job_tests="integration"
+  job_includes="$1"
+  job
+}
+
+# outputs several integration test jobs
+integration_test () {
+  # each test that lasts up to 30 minutes gets its own ci job
+  for tests in $(find_tests tests/integration/30_minutes | batch 1 ","); do
+    integration_test_job $tests
+  done
+
+  # tests that last up to 5 minutes are batched per 6 into a ci job
+  for tests in $(find_tests tests/integration/5_minutes | batch 6 ","); do
+    integration_test_job $tests
+  done
+
+  # tests that last up to 1 minute are batched per 30 into a ci job
+  for tests in $(find_tests tests/integration/1_minute | batch 30 ","); do
+    integration_test_job $tests
+  done
+}
+
+# outputs jobs for all test types
+all_tests () {
+  unit_test
+  contract_test
+  integration_test
+  tools_test
+}
+
+# outputs jobs for all operating systems and all test types
+all_jobs () {
+  echo "["
+  for os in linux macos windows; do
+    $os
+    all_tests
+  done
+  echo "]"
+}
+
+all_jobs
