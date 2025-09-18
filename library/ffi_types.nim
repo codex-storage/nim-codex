273e6e2a9bcc68ef631b3a6d6eaa8937b5336659
Arnaud <arnaud@status.im>
2025-09-18 11:36:31 +0200

    Refactoring

--- library/ffi_types.nim
+++ library/ffi_types.nim
@@ -5,6 +5,7 @@
 
 ################################################################################
 ### Exported types
+import results
 
 type CodexCallback* = proc(
   callerRet: cint, msg: ptr cchar, len: csize_t, userData: pointer
@@ -13,6 +14,31 @@ type CodexCallback* = proc(
 const RET_OK*: cint = 0
 const RET_ERR*: cint = 1
 const RET_MISSING_CALLBACK*: cint = 2
+
+## Returns RET_OK as acknowledgment and call the callback
+## with RET_OK code and the provided message.
+proc success*(callback: CodexCallback, msg: string, userData: pointer): cint =
+  callback(RET_OK, cast[ptr cchar](msg), cast[csize_t](len(msg)), userData)
+
+  return RET_OK
+
+## Returns RET_ERR as acknowledgment and call the callback
+## with RET_ERR code and the provided message.
+proc error*(callback: CodexCallback, msg: string, userData: pointer): cint =
+  let msg = "libcodex error: " & msg
+  callback(RET_ERR, unsafeAddr msg[0], cast[csize_t](len(msg)), userData)
+
+  return RET_ERR
+
+## Returns RET_OK as acknowledgment if the result is ok.
+## If not, return RET_ERR and call the callback with the error message.
+proc okOrError*[T](
+    callback: CodexCallback, res: Result[T, string], userData: pointer
+): cint =
+  if res.isOk:
+    return RET_OK
+
+  return callback.error($res.error, userData)
 
 ### End of exported types
 ################################################################################
