a90f95898a37ca37ed9a124cac0268f56d48bb45
Arnaud <arnaud@status.im>
2025-10-07 08:23:31 +0200

    Export shutdown proc

--- codex/codex.nim
+++ codex/codex.nim
@@ -224,7 +224,7 @@ proc close*(s: CodexServer) {.async.} =
     error "Failed to close codex node", failures = res.failure.len
     raiseAssert "Failed to close codex node"
 
-proc shutdown(server: CodexServer) {.async.} =
+proc shutdown*(server: CodexServer) {.async.} =
   await server.stop()
   await server.close()
 
