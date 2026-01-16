7d6b0a5f0784777a30e65b80402a41eb60e4bdfa
gmega <giuliano.mega@gmail.com>
2026-01-16 14:13:22 -0300

    fix libstorage test

--- examples/c/storage.c
+++ examples/c/storage.c
@@ -665,7 +665,7 @@ int check_download_manifest(void *storage_ctx, const char *cid)
 
     int ret = is_resp_ok(r, &res);
 
-    const char *expected_manifest = "{\"treeCid\":\"zDzSvJTf8JYwvysKPmG7BtzpbiAHfuwFMRphxm4hdvnMJ4XPJjKX\",\"datasetSize\":12,\"blockSize\":65536,\"filename\":\"hello_world.txt\",\"mimetype\":\"text/plain\",\"protected\":false}";
+    const char *expected_manifest = "{\"treeCid\":\"zDzSvJTf8JYwvysKPmG7BtzpbiAHfuwFMRphxm4hdvnMJ4XPJjKX\",\"datasetSize\":12,\"blockSize\":65536,\"filename\":\"hello_world.txt\",\"mimetype\":\"text/plain\"}";
 
     if (strncmp(res, expected_manifest, strlen(expected_manifest)) != 0)
     {
