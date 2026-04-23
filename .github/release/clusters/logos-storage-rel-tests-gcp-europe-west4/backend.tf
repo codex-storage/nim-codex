be1c78fc0bc90cf5a9ba3cde71f321da4839a583
E M <5089238+emizzle@users.noreply.github.com>
2026-04-23 15:49:45 +1000

    rename cluster to match previous change

--- .github/release/clusters/logos-storage-rel-tests-gcp-europe-west4/backend.tf
+++ .github/release/clusters/logos-storage-rel-tests-gcp-europe-west4/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    prefix = "clusters/logos-storage-dist-tests-gcp-europe-west4"
+    prefix = "clusters/logos-storage-rel-tests-gcp-europe-west4"
     # bucket is supplied at init time via:
     #   terraform init -backend-config="bucket=<bucket-name>"
   }
