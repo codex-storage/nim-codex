ca46521a63d281f1a30704a15a72c056e62c3458
Arnaud <arnaud@status.im>
2026-06-04 14:39:17 +0400

    Update Nix configuration

--- nix/nimble.nix
+++ nix/nimble.nix
@@ -4,7 +4,6 @@ let
   tools = pkgs.callPackage ./tools.nix {};
   nbsVersion = tools.findKeyValue "^[[:space:]]+NIMBLE_COMMIT='([a-f0-9]+)'.*$" ../vendor/nimbus-build-system/scripts/build_nim.sh;
   nimVersion = tools.findKeyValue "^ +NimbleStableCommit = \"([a-f0-9]+)\".+" ../vendor/nimbus-build-system/vendor/Nim/koch.nim;
-  sourceFile = ../vendor/nimbus-build-system/vendor/Nim/koch.nim;
 in pkgs.fetchFromGitHub {
   owner = "nim-lang";
   repo = "nimble";
